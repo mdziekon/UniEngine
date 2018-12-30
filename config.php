3835b1a990f4006169c4213ada4a6a00257dd20b
Michal Dziekonski <glibnes@gmail.com>
2018-12-30 05:19:24 +0100

    Uncommit changes to "empty in repo" files (no secrets leaked, accidentally commited from test build)

--- config.php
+++ config.php
@@ -1,12 +1,4 @@
 <?php
 if(!defined("INSIDE")){ die('Access Denied'); }
-$__ServerConnectionSettings = array
-(
-    'server' => 'mysql',
-    'user' => 'root',
-    'pass' => '123456',
-    'name' => 'uniengine',
-    'prefix' => 'uni3_',
-    'secretword' => 'SzkzxlxJd3r9h4Xb8MNm'
-);
+// Config will be available after installation
 ?>
