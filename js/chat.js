3504b897da544c672ab95a024b991151bf5d5ec7
Michal Dziekonski <glibnes@gmail.com>
2019-01-06 02:36:06 +0100

    Update JS port of preg_quote()

--- js/chat.js
+++ js/chat.js
@@ -57,14 +57,24 @@ var YourNicknameReplace_RegExp;
 
 // Functions
 // > Sanitize some data
-function preg_quote (str) {
-    // http://kevin.vanzonneveld.net
-    // + original by: booeyOH
-    // + improved by: Ates Goral (http://magnetiq.com)
-    // + improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // + bugfixed by: Onno Marsman
+function preg_quote (str, delimiter) {
+    //  discuss at: http://locutus.io/php/preg_quote/
+    // original by: booeyOH
+    // improved by: Ates Goral (http://magnetiq.com)
+    // improved by: Kevin van Zonneveld (http://kvz.io)
+    // improved by: Brett Zamir (http://brett-zamir.me)
+    // bugfixed by: Onno Marsman (https://twitter.com/onnomarsman)
+    //   example 1: preg_quote("$40")
+    //   returns 1: '\\$40'
+    //   example 2: preg_quote("*RRRING* Hello?")
+    //   returns 2: '\\*RRRING\\* Hello\\?'
+    //   example 3: preg_quote("\\.+*?[^]$(){}=!<>|:")
+    //   returns 3: '\\\\\\.\\+\\*\\?\\[\\^\\]\\$\\(\\)\\{\\}\\=\\!\\<\\>\\|\\:'
 
-    return (str + "").replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:])/g, "\\$1");
+    return (str + "").replace(
+        new RegExp("[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\" + (delimiter || "") + "-]", "g"),
+        "\\$&"
+    );
 }
 // > Make pretty Time
 function padTime (str) {
