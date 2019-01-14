843683741a0f14ca86b4ffdecf5a199c1e924a21
Michal Dziekonski <glibnes@gmail.com>
2019-01-14 22:26:08 +0100

    BBCode related functions have to ditch PCRE's /e flag as well

--- includes/functions/BBcodeFunction.php
+++ includes/functions/BBcodeFunction.php
@@ -2,82 +2,122 @@
 
 function bbcodeChat($String)
 {
-    $pattern = array
+    $patterns = array
     (
         '/\[b\](.*?)\[\/b\]/is',
         '/\[i\](.*?)\[\/i\]/is',
         '/\[u\](.*?)\[\/u\]/is',
         '/\[s\](.*?)\[\/s\]/is',
-        '/\[url=(.*?)\](.*?)\[\/url\]/ise',
-        '/\[url\](.*?)\[\/url\]/ise',
+        '/\[url=(.*?)\](.*?)\[\/url\]/is',
+        '/\[url\](.*?)\[\/url\]/is',
         '/\[color=(.*?)\](.*?)\[\/color\]/is',
         '/\[coord=([0-9]{1,3}):([0-9]{1,3}):([0-9]{1,3})\]/is',
     );
 
-    $replace = array
+    $replacers = array
     (
         '<b>\1</b>',
         '<i>\1</i>',
         '<u>\1</u>',
         '<span style="text-decoration: line-through;">\1</span>',
-        'urlfix(\'\\1\',\'\\2\')',
-        'urlfix(\'\\1\',\'\\1\')',
+        function ($matches) {
+            return urlfix($matches[1], $matches[2]);
+        },
+        function ($matches) {
+            return urlfix($matches[1], $matches[1]);
+        },
         '<span style="color: \1;">\2</span>',
         '<a href="galaxy.php?mode=3&amp;galaxy=\1&amp;system=\2&amp;planet=\3">[\1:\2:\3]</a>',
     );
 
-    return preg_replace($pattern, $replace, nl2br(htmlspecialchars(stripslashes($String))));
+    $result = nl2br(htmlspecialchars(stripslashes($String)));
+
+    foreach ($patterns as $idx => $pattern) {
+        $replacer = $replacers[$idx];
+
+        if (is_callable($replacer)) {
+            $result = preg_replace_callback($pattern, $replacer, $result);
+        } else {
+            $result = preg_replace($pattern, $replacer, $result);
+        }
+    }
+
+    return $result;
 }
 
 function bbcode($string)
 {
-    $pattern = array
+    $patterns = array
     (
         '/\\n/',
         '/\\r/',
-        '/\[list\](.*?)\[\/list\]/ise',
+        '/\[list\](.*?)\[\/list\]/is',
         '/\[b\](.*?)\[\/b\]/is',
         '/\[strong\](.*?)\[\/strong\]/is',
         '/\[i\](.*?)\[\/i\]/is',
         '/\[u\](.*?)\[\/u\]/is',
         '/\[s\](.*?)\[\/s\]/is',
         '/\[del\](.*?)\[\/del\]/is',
-        '/\[url=(.*?)\](.*?)\[\/url\]/ise',
-        '/\[url\](.*?)\[\/url\]/ise',
+        '/\[url=(.*?)\](.*?)\[\/url\]/is',
+        '/\[url\](.*?)\[\/url\]/is',
         '/\[email=(.*?)\](.*?)\[\/email\]/is',
-        '/\[img](.*?)\[\/img\]/ise',
+        '/\[img](.*?)\[\/img\]/is',
         '/\[color=(.*?)\](.*?)\[\/color\]/is',
-        '/\[quote\](.*?)\[\/quote\]/ise',
-        '/\[code\](.*?)\[\/code\]/ise',
+        '/\[quote\](.*?)\[\/quote\]/is',
+        '/\[code\](.*?)\[\/code\]/is',
         '/\[size=(.*?)\](.*?)\[\/size\]/is',
         '/\[background color=(.*?)\](.*?)\[\/background\]/is',
         '/\[coord=([0-9]{1,3}):([0-9]{1,3}):([0-9]{1,3})\]/is',
     );
 
-    $replace = array
+    $replacers = array
     (
         '',
         '',
-        'sList(\'\\1\')',
+        function ($matches) {
+            return sList($matches[1]);
+        },
         '<b>\1</b>',
         '<strong>\1</strong>',
         '<i>\1</i>',
         '<span style="text-decoration: underline;">\1</span>',
         '<span style="text-decoration: line-through;">\1</span>',
         '<span style="text-decoration: line-through;">\1</span>',
-        'urlfix(\'\\1\',\'\\2\')',
-        'urlfix(\'\\1\',\'\\1\')',
+        function ($matches) {
+            return urlfix($matches[1], $matches[2]);
+        },
+        function ($matches) {
+            return urlfix($matches[1], $matches[1]);
+        },
         '<a href="mailto:\1" title="\1">\2</a>',
-        'imagefix(\'\\1\')',
+        function ($matches) {
+            return imagefix($matches[1]);
+        },
         '<span style="color: \1;">\2</span>',
-        'sQuote(\'\1\')',
-        'sCode(\'\1\')',
+        function ($matches) {
+            return sQuote($matches[1]);
+        },
+        function ($matches) {
+            return sCode($matches[1]);
+        },
         '<font size="\1">\2</font>',
         '<div style="background-color: \1">\2</div>',
         '<a href="galaxy.php?mode=3&amp;galaxy=\1&amp;system=\2&amp;planet=\3">[\1:\2:\3]</a>',
     );
 
-    return preg_replace($pattern, $replace, nl2br(htmlspecialchars(stripslashes($string))));
+    $result = nl2br(htmlspecialchars(stripslashes($string)));
+
+    foreach ($patterns as $idx => $pattern) {
+        $replacer = $replacers[$idx];
+
+        if (is_callable($replacer)) {
+            $result = preg_replace_callback($pattern, $replacer, $result);
+        } else {
+            $result = preg_replace($pattern, $replacer, $result);
+        }
+    }
+
+    return $result;
 }
 
 function image($string)
