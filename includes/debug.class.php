46da11e5625d466a76f431875683beaa81eb4df4
Michal Dziekonski <glibnes@gmail.com>
2019-01-15 02:19:38 +0100

    Formatting improvements in debug.class.php file

--- includes/debug.class.php
+++ includes/debug.class.php
@@ -6,40 +6,45 @@ class debug
 {
     var $NestingPrevention = 0;
     var $PreviousMessage = '';
-    function error($message)
-    {
+
+    function error($message) {
         global $_DBLink, $_User, $_EnginePath;
 
         $this->NestingPrevention += 1;
-        if($this->NestingPrevention > 1)
-        {
-            trigger_error('<b>ErrorNesting Prevention!</b><br/>'.$this->PreviousMessage, E_USER_ERROR);
+
+        if ($this->NestingPrevention > 1) {
+            trigger_error(
+                (
+                    '<b>ErrorNesting Prevention!</b><br/>' .
+                    $this->PreviousMessage
+                ),
+                E_USER_ERROR
+            );
         }
         define('IN_ERROR', true);
 
-        if(LOCALHOST)
-        {
-            require($_EnginePath.'config.localhost.php');
-        }
-        else if(TESTSERVER)
-        {
-            require($_EnginePath.'config.testserver.php');
-        }
-        else
-        {
-            require($_EnginePath.'config.php');
+        if (LOCALHOST) {
+            require($_EnginePath . 'config.localhost.php');
+        } else if (TESTSERVER) {
+            require($_EnginePath . 'config.testserver.php');
+        } else {
+            require($_EnginePath . 'config.php');
         }
 
-        if(!$_DBLink)
-        {
+        if (!$_DBLink) {
             trigger_error('DBDriver Connection Error #01<br/>', E_USER_ERROR);
         }
-        if(empty($_User['id']))
-        {
+        if (empty($_User['id'])) {
             $_User['id'] = '0';
         }
-        $Replace_Search        = array('{{table}}', '{{prefix}}');
-        $Replace_Replace    = array($__ServerConnectionSettings['prefix'].'errors', $__ServerConnectionSettings['prefix']);
+        $Replace_Search = [
+            '{{table}}',
+            '{{prefix}}'
+        ];
+        $Replace_Replace = [
+            $__ServerConnectionSettings['prefix'] . 'errors',
+            $__ServerConnectionSettings['prefix']
+        ];
 
         $EscapedMessage = $_DBLink->escape_string($message);
 
@@ -70,12 +75,9 @@ class debug
         $ErrorMsg = 'An Error occured!<br/>Error ID: <b>'. $ErrorID .'</b>';
         $this->PreviousMessage = $ErrorMsg;
 
-        if(!function_exists('message'))
-        {
+        if (!function_exists('message')) {
             echo $ErrorMsg;
-        }
-        else
-        {
+        } else {
             message($ErrorMsg, 'System Error!');
         }
 
