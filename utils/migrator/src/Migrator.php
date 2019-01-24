9856f3c95fd92573350b395a4626aadfbdca370e
Michal Dziekonski <glibnes@gmail.com>
2019-01-25 00:17:33 +0100

    Implement basic migratior entries loader

--- utils/migrator/src/Migrator.php
+++ utils/migrator/src/Migrator.php
@@ -43,6 +43,44 @@ class Migrator {
     private function getRealPath($path) {
         return ($this->rootPath . $path);
     }
+
+    private function loadMigrationEntries() {
+        $migrationsPath = $this->getRealPath("./migrations");
+
+        $list = scandir($migrationsPath);
+
+        if ($list === false) {
+            throw new FileIOException("Could not load migrations directory");
+        }
+
+        $migrationFiles = array_filter($list, function ($file) {
+            // Migration scripts' filenames follow this pattern:
+            // <4 digit year><2 digit month><2 digit day>_<2 digit hour><2 digit minute><2 digit second>_<short description>.php
+            $isMatching = preg_match("/^\d{8}_\d{6}_.*?\.php$/", $file);
+
+            return ($isMatching === 1);
+        });
+
+        return array_map(function ($file) {
+            preg_match(
+                "/^(\d{8}_\d{6})_(.*?)\.php$/",
+                $file,
+                $matches
+            );
+
+            $datetime = \DateTime::createFromFormat(
+                "Ymd_His",
+                $matches[1]
+            );
+
+            return [
+                "filename" => $file,
+                "id" => $datetime->format("Ymd_His"),
+                "datetime" => $datetime,
+                "desc" => $matches[2]
+            ];
+        }, $migrationFiles);
+    }
 }
 
 ?>
