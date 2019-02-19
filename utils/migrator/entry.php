69fc198575a605a175ff5318f507f157d3bfd85b
Michal Dziekonski <glibnes@gmail.com>
2019-02-19 03:20:44 +0100

    Migrator should take commands and be able to generate new migration files

--- utils/migrator/entry.php
+++ utils/migrator/entry.php
@@ -5,15 +5,37 @@ require_once(__DIR__ . "/autoload.php");
 use \UniEngine\Utils\Migrations as Migrations;
 
 if (PHP_SAPI !== "cli") {
-    throw new Exception("Migrator utility can only be run from 'cli' environment");
+    throw new \Exception("Migrator utility can only be run from 'cli' environment");
 }
 
 $migrator = new Migrations\Migrator([
     "rootPath" => "./"
 ]);
 
-$migrator->runMigration([
-    "wasManualActionConfirmed" => in_array("--confirm-manual-action", $argv)
-]);
+$cmd = $argv[1];
+
+switch ($cmd) {
+    case "migrate:run":
+        $migrator->runMigration([
+            "wasManualActionConfirmed" => in_array(
+                "--confirm-manual-action",
+                $argv
+            )
+        ]);
+
+        break;
+    case "migrate:make":
+        $migrator->generateNewMigration([
+            "name" => (
+                !empty($argv[2]) ?
+                $argv[2] :
+                null
+            )
+        ]);
+
+        break;
+    default:
+        throw new \Exception("Invalid command \"{$cmd}\"");
+}
 
 ?>
