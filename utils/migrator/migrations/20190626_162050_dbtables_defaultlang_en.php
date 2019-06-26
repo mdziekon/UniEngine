c4f40e321bccb4d50f458f77db3d02c4fa04a4da
Michal Dziekonski <glibnes@gmail.com>
2019-06-26 16:12:17 +0200

    When installing the DB, default to en instead of pl

---
+++ utils/migrator/migrations/20190626_162050_dbtables_defaultlang_en.php
@@ -0,0 +1,57 @@
+<?php
+
+use \UniEngine\Utils\Migrations as Migrations;
+
+/**
+ * Changelog:
+ *  - Changes the default value of language to "en" (previously was "pl")
+ *
+ */
+class Migration_20190626_162050 implements Migrations\Interfaces\Migration {
+    protected $configProvider;
+
+    public function __construct() {
+        $this->configProvider = new Migrations\PHPConfigProvider();
+    }
+
+    public function up() {
+        $db = $this->getDBDriver();
+
+        $db->executeQuery(
+            "ALTER TABLE `{{table(users)}}` " .
+            "ALTER COLUMN `lang` " .
+            "SET DEFAULT 'en';"
+        );
+    }
+
+    public function down() {
+        $db = $this->getDBDriver();
+
+        $db->executeQuery(
+            "ALTER TABLE `{{table(users)}}` " .
+            "ALTER COLUMN `lang` " .
+            "SET DEFAULT 'pl';"
+        );
+    }
+
+    public function isPriorManualActionRequired() {
+        return false;
+    }
+
+    public function getPreviousProjectVersion() {
+        return "1.0.0";
+    }
+
+    private function getDBDriver() {
+        $self = $this;
+        $db = new Migrations\DBDriver([
+            "configProvider" => function () use ($self) {
+                return $self->configProvider->getDatabaseConfig();
+            }
+        ]);
+
+        return $db;
+    }
+}
+
+?>
