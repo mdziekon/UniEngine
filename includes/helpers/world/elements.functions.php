d3168f12c56f8d423c65326e070e15f4d370b8e2
Michal Dziekonski <glibnes@gmail.com>
2019-08-28 23:34:53 +0200

    Define more "type-checking" helpers

--- includes/helpers/world/elements.functions.php
+++ includes/helpers/world/elements.functions.php
@@ -8,16 +8,42 @@ function isStructure($elementID) {
     return in_array($elementID, $_Vars_ElementCategories['build']);
 }
 
-function isStorageStructure($elementID) {
+function isTechnology($elementID) {
     global $_Vars_ElementCategories;
 
-    return in_array($elementID, $_Vars_ElementCategories['storages']);
+    return in_array($elementID, $_Vars_ElementCategories['tech']);
 }
 
 function isShip($elementID) {
     global $_Vars_ElementCategories;
 
     return in_array($elementID, $_Vars_ElementCategories['fleet']);
+}
+
+function isDefenseSystem($elementID) {
+    global $_Vars_ElementCategories;
+
+    return in_array($elementID, $_Vars_ElementCategories['defense']);
+}
+
+function isMissile($elementID) {
+    global $_Vars_ElementCategories;
+
+    return in_array($elementID, $_Vars_ElementCategories['rockets']);
+}
+
+function isStorageStructure($elementID) {
+    global $_Vars_ElementCategories;
+
+    return in_array($elementID, $_Vars_ElementCategories['storages']);
+}
+
+function isConstructibleInHangar($elementID) {
+    return (
+        isShip($elementID) ||
+        isDefenseSystem($elementID) ||
+        isMissile($elementID)
+    );
 }
 
 ?>
