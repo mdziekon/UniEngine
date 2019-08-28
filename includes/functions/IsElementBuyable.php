ee7d671b032ac77a9c02095e91c8f65bb96822eb
Michal Dziekonski <glibnes@gmail.com>
2019-08-28 23:41:34 +0200

    Use the new cost calculator in IsElementBuyable

--- includes/functions/IsElementBuyable.php
+++ includes/functions/IsElementBuyable.php
@@ -1,61 +1,30 @@
 <?php
 
+use UniEngine\Engine\Includes\Helpers\World\Elements;
+
 function IsElementBuyable($TheUser, $ThePlanet, $ElementID, $Incremental = true, $ForDestroy = false, $GetPremiumData = false)
 {
-    global $_Vars_Prices, $_Vars_GameElements, $_Vars_ElementCategories;
-
     if(isOnVacation($TheUser))
     {
         return false;
     }
 
-    if($Incremental)
-    {
-        $level = 0;
-        if(in_array($ElementID, $_Vars_ElementCategories['tech']))
-        {
-            if(isset($TheUser[$_Vars_GameElements[$ElementID]]))
-            {
-                $level = $TheUser[$_Vars_GameElements[$ElementID]];
-            }
-        }
-        else
-        {
-            if(isset($ThePlanet[$_Vars_GameElements[$ElementID]]))
-            {
-                $level = $ThePlanet[$_Vars_GameElements[$ElementID]];
-            }
-        }
-    }
-    if($ForDestroy === true)
-    {
-        $level -= 1;
-    }
+    $elementCost = Elements\calculatePurchaseCost(
+        $ElementID,
+        $ThePlanet,
+        $TheUser,
+        [
+            'purchaseMode' => (
+                !$ForDestroy ?
+                Elements\PurchaseMode::Upgrade :
+                Elements\PurchaseMode::Downgrade
+            )
+        ]
+    );
 
-    $array = array('metal', 'crystal', 'deuterium', 'energy_max');
-
-    foreach($array as $ResType)
-    {
-        if(isset($_Vars_Prices[$ElementID][$ResType]) && $_Vars_Prices[$ElementID][$ResType] > 0)
-        {
-            if($Incremental)
-            {
-                $cost[$ResType] = floor($_Vars_Prices[$ElementID][$ResType] * pow($_Vars_Prices[$ElementID]['factor'], $level));
-            }
-            else
-            {
-                $cost[$ResType] = floor($_Vars_Prices[$ElementID][$ResType]);
-            }
-
-            if($ForDestroy)
-            {
-                $cost[$ResType] = floor($cost[$ResType] / 2);
-            }
-
-            if($cost[$ResType] > $ThePlanet[$ResType])
-            {
-                return false;
-            }
+    foreach ($elementCost as $costResourceKey => $costValue) {
+        if ($costValue > $ThePlanet[$costResourceKey]) {
+            return false;
         }
     }
 
