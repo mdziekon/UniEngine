8b1f3925a10d68581439265911e5d026e9fe8b53
Michal Dziekonski <glibnes@gmail.com>
2019-09-07 23:16:05 +0200

    Decouple structure command handling from page rendering

--- includes/functions/StructuresBuildingPage.php
+++ includes/functions/StructuresBuildingPage.php
@@ -1,5 +1,207 @@
 <?php
 
+use UniEngine\Engine\Includes\Helpers\World\Elements;
+
+//  Arguments:
+//      - $user
+//      - $planet
+//      - $input
+//      - $params (Object)
+//          - timestamp (Number)
+//
+function _handleStructureCommand(&$user, &$planet, &$input, $params) {
+    $knownCommands = [
+        'cancel',
+        'remove',
+        'insert',
+        'destroy'
+    ];
+
+    $timestamp = $params['timestamp'];
+
+    $cmd = (
+        isset($input['cmd']) ?
+        $input['cmd'] :
+        null
+    );
+
+    if (!in_array($cmd, $knownCommands)) {
+        return [
+            'isSuccess' => false,
+            'error' => [
+                'invalidCommand' => true
+            ]
+        ];
+    }
+
+    if (isOnVacation($user)) {
+        return [
+            'isSuccess' => false,
+            'error' => [
+                'onVacation' => true
+            ]
+        ];
+    }
+
+    switch ($cmd) {
+        case "cancel":
+            $cmdResult = _handleStructureCommandCancel($user, $planet);
+
+            break;
+        case 'remove':
+            $cmdResult = _handleStructureCommandRemove($user, $planet, $input);
+
+            break;
+        case "insert":
+        case "destroy":
+            $cmdResult = _handleStructureCommandInsert($user, $planet, $input, [
+                'cmd' => $cmd
+            ]);
+
+            break;
+    }
+
+    if (!$cmdResult['isSuccess']) {
+        return [
+            'isSuccess' => false,
+            'error' => $cmdResult['error']
+        ];
+    }
+
+    $isPlanetRecordSyncNotNeeded = HandlePlanetQueue_StructuresSetNext(
+        $planet,
+        $user,
+        $timestamp,
+        true
+    );
+
+    if (!$isPlanetRecordSyncNotNeeded) {
+        include($_EnginePath . 'includes/functions/BuildingSavePlanetRecord.php');
+
+        BuildingSavePlanetRecord($planet);
+    }
+
+    return [
+        'isSuccess' => true,
+        'payload' => [
+            'elementID' => $cmdResult['payload']['elementID']
+        ]
+    ];
+}
+
+function _handleStructureCommandCancel(&$user, &$planet) {
+    global $_EnginePath;
+
+    include($_EnginePath . 'includes/functions/CancelBuildingFromQueue.php');
+
+    $highlightElementID = CancelBuildingFromQueue($planet, $user);
+
+    return [
+        'isSuccess' => true,
+        'payload' => [
+            'elementID' => $highlightElementID
+        ]
+    ];
+}
+
+function _handleStructureCommandRemove(&$user, &$planet, &$input) {
+    global $_EnginePath;
+
+    $listElementIdx = (
+        isset($input['listid']) ?
+        intval($input['listid']) :
+        -1
+    );
+
+    if ($listElementIdx <= 0) {
+        return [
+            'isSuccess' => false,
+            'error' => [
+                'invalidListIndex' => true
+            ]
+        ];
+    }
+
+    include($_EnginePath . 'includes/functions/RemoveBuildingFromQueue.php');
+
+    $highlightElementID = RemoveBuildingFromQueue($planet, $user, $listElementIdx);
+
+    return [
+        'isSuccess' => true,
+        'payload' => [
+            'elementID' => $highlightElementID
+        ]
+    ];
+}
+
+//  Arguments:
+//      - $user
+//      - $planet
+//      - $input
+//      - $params (Object)
+//          - cmd (EnumString: 'insert' | 'destroy')
+//
+function _handleStructureCommandInsert(&$user, &$planet, &$input, $params) {
+    global $_EnginePath, $_GameConfig;
+
+    $cmd = $params['cmd'];
+
+    $elementID = (
+        isset($input['building']) ?
+        intval($input['building']) :
+        -1
+    );
+
+    if (!Elements\isStructure($elementID)) {
+        return [
+            'isSuccess' => false,
+            'error' => [
+                'invalidElementID' => true
+            ]
+        ];
+    }
+
+    if (!Elements\isStructureAvailableOnPlanetType($elementID, $planet['planet_type'])) {
+        return [
+            'isSuccess' => false,
+            'error' => [
+                'structureUnavailable' => true
+            ]
+        ];
+    }
+
+    if (
+        $elementID == 31 &&
+        $planet['techQueue_Planet'] > 0 &&
+        $planet['techQueue_EndTime'] > 0 &&
+        $_GameConfig['BuildLabWhileRun'] != 1
+    ) {
+        return [
+            'isSuccess' => false,
+            'error' => [
+                'researchInProgress' => true
+            ]
+        ];
+    }
+
+    $purchaseMode = (
+        $cmd === 'insert' ?
+        true :
+        false
+    );
+
+    include($_EnginePath.'includes/functions/AddBuildingToQueue.php');
+
+    AddBuildingToQueue($planet, $user, $elementID, $purchaseMode);
+
+    return [
+        'isSuccess' => true,
+        'payload' => [
+            'elementID' => $elementID
+        ]
+    ];
+}
+
 function StructuresBuildingPage(&$CurrentPlanet, $CurrentUser)
 {
     global    $_Lang, $_SkinPath, $_GameConfig, $_GET, $_EnginePath,
@@ -38,75 +240,17 @@ function StructuresBuildingPage(&$CurrentPlanet, $CurrentUser)
     $TPL['infobox_req_selector_dual']    = gettemplate('buildings_compact_infobox_req_selector_dual');
 
     // Handle Commands
-    if(!isOnVacation($CurrentUser))
-    {
-        if(!empty($_GET['cmd']))
-        {
-            $TheCommand = $_GET['cmd'];
-            if($TheCommand == 'cancel')
-            {
-                include($_EnginePath.'includes/functions/CancelBuildingFromQueue.php');
-                $ShowID = CancelBuildingFromQueue($CurrentPlanet, $CurrentUser);
-                $CommandDone = true;
-            }
-            else if($TheCommand == 'remove')
-            {
-                if(!empty($_GET['listid']))
-                {
-                    include($_EnginePath.'includes/functions/RemoveBuildingFromQueue.php');
-                    $ShowID = RemoveBuildingFromQueue($CurrentPlanet, $CurrentUser, intval($_GET['listid']));
-                    $CommandDone = true;
-                }
-            }
-            else if($TheCommand == 'insert' OR $TheCommand == 'destroy')
-            {
-                if(!empty($_GET['building']))
-                {
-                    $ElementID = intval($_GET['building']);
-                    if(in_array($ElementID, $_Vars_ElementCategories['buildOn'][$CurrentPlanet['planet_type']]))
-                    {
-                        if($TheCommand == 'insert')
-                        {
-                            $AddMode = true;
-                        }
-                        else
-                        {
-                            $AddMode = false;
-                        }
-                        if($ElementID == 31 AND $CurrentUser['techQueue_Planet'] > 0 AND $CurrentUser['techQueue_EndTime'] > 0 AND $_GameConfig['BuildLabWhileRun'] != 1)
-                        {
-                            $BlockCommand = true;
-                        }
-                        else
-                        {
-                            $BlockCommand = false;
-                        }
-                        if($BlockCommand !== true)
-                        {
-                            include($_EnginePath.'includes/functions/AddBuildingToQueue.php');
-                            AddBuildingToQueue($CurrentPlanet, $CurrentUser, $ElementID, $AddMode);
-                            $CommandDone = true;
-                        }
+    $cmdResult = _handleStructureCommand(
+        $CurrentUser,
+        $CurrentPlanet,
+        $_GET,
+        [
+            "timestamp" => $Now
+        ]
+    );
 
-                        $ShowID = $ElementID;
-                    }
-                }
-            }
-
-            if($ShowID > 0)
-            {
-                $ShowElementID = $ShowID;
-            }
-
-            if($CommandDone === true)
-            {
-                if(HandlePlanetQueue_StructuresSetNext($CurrentPlanet, $CurrentUser, $Now, true) === false)
-                {
-                    include($_EnginePath.'includes/functions/BuildingSavePlanetRecord.php');
-                    BuildingSavePlanetRecord($CurrentPlanet);
-                }
-            }
-        }
+    if ($cmdResult['isSuccess']) {
+        $ShowElementID = $cmdResult['payload']['elementID'];
     }
     // End of - Handle Commands
 
