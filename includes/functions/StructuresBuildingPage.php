e36f41fde337c5c58b26519b4e15042fe62d3d89
Michal Dziekonski <glibnes@gmail.com>
2019-09-10 15:35:28 +0200

    Remove unused globals

--- includes/functions/StructuresBuildingPage.php
+++ includes/functions/StructuresBuildingPage.php
@@ -208,7 +208,7 @@ function _handleStructureCommandInsert(&$user, &$planet, &$input, $params) {
 function StructuresBuildingPage(&$CurrentPlanet, $CurrentUser)
 {
     global    $_Lang, $_SkinPath, $_GameConfig, $_GET, $_EnginePath,
-            $_Vars_GameElements, $_Vars_ElementCategories, $_Vars_MaxElementLevel, $_Vars_PremiumBuildings, $_Vars_IndestructibleBuildings;
+            $_Vars_GameElements, $_Vars_ElementCategories, $_Vars_IndestructibleBuildings;
 
     include($_EnginePath.'includes/functions/GetElementTechReq.php');
     includeLang('worldElements.detailed');
