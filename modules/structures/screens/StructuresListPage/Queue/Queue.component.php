cbd30c588395988124a5ee100db0d81a9e442816
Michal Dziekonski <glibnes@gmail.com>
2019-09-17 17:34:07 +0200

    Simplify structures queue template data filling loop

--- modules/structures/screens/StructuresListPage/Queue/Queue.component.php
+++ modules/structures/screens/StructuresListPage/Queue/Queue.component.php
@@ -39,7 +39,6 @@ function render ($props) {
         'queue_elements_next' => gettemplate('buildings_compact_queue_nextel')
     ];
 
-    $queueUnfinishedLenght = 0;
     $queuedResourcesToUse = [
         'metal' => 0,
         'crystal' => 0,
@@ -50,146 +49,142 @@ function render ($props) {
 
     $buildingsQueue = Planets\Queues\parseStructuresQueueString($planet['buildQueue']);
 
-    if (!empty($buildingsQueue)) {
-        $queueElementsTplData = [];
-        $queueDisplayIdx = 0;
+    $queueElementsTplData = [];
+    $queueUnfinishedElementsCount = 0;
 
-        foreach ($buildingsQueue as $queueIdx => $queueElement) {
-            if ($queueElement['endTimestamp'] < $currentTimestamp) {
-                continue;
-            }
-
-            $listID = $queueDisplayIdx + 1;
-            $elementID = $queueElement['elementID'];
-            $elementLevel = $queueElement['level'];
-            $progressDuration = $queueElement['duration'];
-            $progressEndTime = $queueElement['endTimestamp'];
-            $progressTimeLeft = $progressEndTime - $currentTimestamp;
-            $isUpgrading = (
-                $queueElement['mode'] == 'build' ?
-                true :
-                false
-            );
-            $isFirstQueueElement = ($queueIdx === 0);
-
-            if ($queueElement['mode'] != 'build') {
-                $elementLevel += 1;
-            }
-
-            $queueElementTplData = [
-                'ListID'                => $listID,
-                'ElementNo'             => $listID,
-                'ElementID'             => $elementID,
-                'Name'                  => $_Lang['tech'][$elementID],
-                'Level'                 => $elementLevel,
-                'PlanetID'              => $planet['id'],
-                'BuildTime'             => pretty_time($progressDuration),
-                'EndTimer'              => pretty_time($progressTimeLeft, true),
-                'EndTimeExpand'         => date('H:i:s', $progressEndTime),
-                'EndDate'               => date('d/m | H:i:s', $progressEndTime),
-                'EndDateExpand'         => prettyDate('d m Y', $progressEndTime, 1),
-
-                'ChronoAppletScript'    => '',
-                'Data_CancelLock_class' => (
-                    Elements\isCancellableOnceInProgress($elementID) ?
-                    '' :
-                    'premblock'
-                ),
-                'ModeText'              => (
-                    $isUpgrading ?
-                    $_Lang['Queue_Mode_Build_1'] :
-                    $_Lang['Queue_Mode_Destroy_1']
-                ),
-                'ModeColor'             => (
-                    $isUpgrading ?
-                    'lime' :
-                    'red'
-                ),
-                'Lang_CancelBtn_Text'   => (
-                    $isFirstQueueElement ?
-                    (
-                        (!Elements\isCancellableOnceInProgress($elementID)) ?
-                        $_Lang['Queue_Cancel_CantCancel'] :
-                        (
-                            $isUpgrading ?
-                            $_Lang['Queue_Cancel_Build'] :
-                            $_Lang['Queue_Cancel_Destroy']
-                        )
-                    ) :
-                    $_Lang['Queue_Cancel_Remove']
-                ),
-                'InfoBox_BuildTime'     => (
-                    $isUpgrading ?
-                    $_Lang['InfoBox_BuildTime'] :
-                    $_Lang['InfoBox_DestroyTime']
-                ),
-
-                'SkinPath'              => $_SkinPath,
-                'LevelText'             => $_Lang['level'],
-                'EndText'               => $_Lang['Queue_EndTime'],
-                'EndTitleBeg'           => $_Lang['Queue_EndTitleBeg'],
-                'EndTitleHour'          => $_Lang['Queue_EndTitleHour'],
-            ];
-
-            if ($isFirstQueueElement) {
-                include_once($_EnginePath . '/includes/functions/InsertJavaScriptChronoApplet.php');
-
-                $queueElementTplData['ChronoAppletScript'] = InsertJavaScriptChronoApplet(
-                    'QueueFirstTimer',
-                    '',
-                    $progressEndTime,
-                    true,
-                    false,
-                    'function() { onQueuesFirstElementFinished(); }'
-                );
-            }
-
-            $queueElementsTplData[] = $queueElementTplData;
-            $elementPlanetKey = _getElementPlanetKey($elementID);
-
-            if (!$isFirstQueueElement) {
-                $temporaryLevelModifier = (
-                    isset($queuedElementLevelModifiers[$elementID]) ?
-                    $queuedElementLevelModifiers[$elementID] :
-                    0
-                );
-
-                $planet[$elementPlanetKey] += $temporaryLevelModifier;
-
-                $elementCost = GetBuildingPrice($user, $planet, $elementID, true, !$isUpgrading);
-                $queuedResourcesToUse['metal'] += $elementCost['metal'];
-                $queuedResourcesToUse['crystal'] += $elementCost['crystal'];
-                $queuedResourcesToUse['deuterium'] += $elementCost['deuterium'];
-
-                $planet[$elementPlanetKey] -= $temporaryLevelModifier;
-            }
-
-            if (!isset($queuedElementLevelModifiers[$elementID])) {
-                $queuedElementLevelModifiers[$elementID] = 0;
-            }
-
-            if (!$isUpgrading) {
-                $queuedElementLevelModifiers[$elementID] -= 1;
-                $fieldsModifierByQueuedDowngrades += 2;
-            } else {
-                $queuedElementLevelModifiers[$elementID] += 1;
-            }
-
-            $queueDisplayIdx += 1;
+    foreach ($buildingsQueue as $queueIdx => $queueElement) {
+        if ($queueElement['endTimestamp'] < $currentTimestamp) {
+            continue;
         }
 
-        $queueUnfinishedLenght = $queueDisplayIdx;
+        $listID = ($queueUnfinishedElementsCount + 1);
+        $elementID = $queueElement['elementID'];
+        $elementLevel = $queueElement['level'];
+        $progressDuration = $queueElement['duration'];
+        $progressEndTime = $queueElement['endTimestamp'];
+        $progressTimeLeft = $progressEndTime - $currentTimestamp;
+        $isUpgrading = (
+            $queueElement['mode'] == 'build' ?
+            true :
+            false
+        );
+        $isFirstQueueElement = ($queueIdx === 0);
 
-        if (!empty($queueElementsTplData)) {
-            foreach ($queueElementsTplData as $elementIdx => $queueElementTplData) {
-                $queueElementTPLBody = (
-                    $elementIdx === 0 ?
-                    $tplBodyCache['queue_elements_first'] :
-                    $tplBodyCache['queue_elements_next']
-                );
+        if ($queueElement['mode'] != 'build') {
+            $elementLevel += 1;
+        }
 
-                $componentTPLData['queueElements'][] = parsetemplate($queueElementTPLBody, $queueElementTplData);
-            }
+        $queueElementTplData = [
+            'ListID'                => $listID,
+            'ElementNo'             => $listID,
+            'ElementID'             => $elementID,
+            'Name'                  => $_Lang['tech'][$elementID],
+            'Level'                 => $elementLevel,
+            'PlanetID'              => $planet['id'],
+            'BuildTime'             => pretty_time($progressDuration),
+            'EndTimer'              => pretty_time($progressTimeLeft, true),
+            'EndTimeExpand'         => date('H:i:s', $progressEndTime),
+            'EndDate'               => date('d/m | H:i:s', $progressEndTime),
+            'EndDateExpand'         => prettyDate('d m Y', $progressEndTime, 1),
+
+            'ChronoAppletScript'    => '',
+            'Data_CancelLock_class' => (
+                Elements\isCancellableOnceInProgress($elementID) ?
+                '' :
+                'premblock'
+            ),
+            'ModeText'              => (
+                $isUpgrading ?
+                $_Lang['Queue_Mode_Build_1'] :
+                $_Lang['Queue_Mode_Destroy_1']
+            ),
+            'ModeColor'             => (
+                $isUpgrading ?
+                'lime' :
+                'red'
+            ),
+            'Lang_CancelBtn_Text'   => (
+                $isFirstQueueElement ?
+                (
+                    (!Elements\isCancellableOnceInProgress($elementID)) ?
+                    $_Lang['Queue_Cancel_CantCancel'] :
+                    (
+                        $isUpgrading ?
+                        $_Lang['Queue_Cancel_Build'] :
+                        $_Lang['Queue_Cancel_Destroy']
+                    )
+                ) :
+                $_Lang['Queue_Cancel_Remove']
+            ),
+            'InfoBox_BuildTime'     => (
+                $isUpgrading ?
+                $_Lang['InfoBox_BuildTime'] :
+                $_Lang['InfoBox_DestroyTime']
+            ),
+
+            'SkinPath'              => $_SkinPath,
+            'LevelText'             => $_Lang['level'],
+            'EndText'               => $_Lang['Queue_EndTime'],
+            'EndTitleBeg'           => $_Lang['Queue_EndTitleBeg'],
+            'EndTitleHour'          => $_Lang['Queue_EndTitleHour'],
+        ];
+
+        if ($isFirstQueueElement) {
+            include_once($_EnginePath . '/includes/functions/InsertJavaScriptChronoApplet.php');
+
+            $queueElementTplData['ChronoAppletScript'] = InsertJavaScriptChronoApplet(
+                'QueueFirstTimer',
+                '',
+                $progressEndTime,
+                true,
+                false,
+                'function() { onQueuesFirstElementFinished(); }'
+            );
+        }
+
+        $queueElementsTplData[] = $queueElementTplData;
+        $elementPlanetKey = _getElementPlanetKey($elementID);
+
+        if (!$isFirstQueueElement) {
+            $temporaryLevelModifier = (
+                isset($queuedElementLevelModifiers[$elementID]) ?
+                $queuedElementLevelModifiers[$elementID] :
+                0
+            );
+
+            $planet[$elementPlanetKey] += $temporaryLevelModifier;
+
+            $elementCost = GetBuildingPrice($user, $planet, $elementID, true, !$isUpgrading);
+            $queuedResourcesToUse['metal'] += $elementCost['metal'];
+            $queuedResourcesToUse['crystal'] += $elementCost['crystal'];
+            $queuedResourcesToUse['deuterium'] += $elementCost['deuterium'];
+
+            $planet[$elementPlanetKey] -= $temporaryLevelModifier;
+        }
+
+        if (!isset($queuedElementLevelModifiers[$elementID])) {
+            $queuedElementLevelModifiers[$elementID] = 0;
+        }
+
+        if (!$isUpgrading) {
+            $queuedElementLevelModifiers[$elementID] -= 1;
+            $fieldsModifierByQueuedDowngrades += 2;
+        } else {
+            $queuedElementLevelModifiers[$elementID] += 1;
+        }
+
+        $queueUnfinishedElementsCount += 1;
+    }
+
+    if (!empty($queueElementsTplData)) {
+        foreach ($queueElementsTplData as $elementIdx => $queueElementTplData) {
+            $queueElementTPLBody = (
+                $elementIdx === 0 ?
+                $tplBodyCache['queue_elements_first'] :
+                $tplBodyCache['queue_elements_next']
+            );
+
+            $componentTPLData['queueElements'][] = parsetemplate($queueElementTPLBody, $queueElementTplData);
         }
     } else {
         $componentTPLData['queueElements'][] = parsetemplate(
@@ -201,7 +196,7 @@ function render ($props) {
     }
 
     $isQueueFull = (
-        $queueUnfinishedLenght >=
+        $queueUnfinishedElementsCount >=
         Users\getMaxStructuresQueueLength($user)
     );
 
@@ -228,7 +223,7 @@ function render ($props) {
             'queuedResourcesToUse' => $queuedResourcesToUse,
             'queuedElementLevelModifiers' => $queuedElementLevelModifiers,
             'fieldsModifier' => $fieldsModifierByQueuedDowngrades,
-            'unfinishedLength' => $queueUnfinishedLenght
+            'unfinishedLength' => $queueUnfinishedElementsCount
         ]
     ];
 }
