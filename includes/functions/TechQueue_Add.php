44105fb9caa500cb3df977274cc36f4422683cae
Michal Dziekonski <glibnes@gmail.com>
2019-10-20 00:16:47 +0200

    TechQueue_Add no longer has to check queue's length, leave it up to validators

--- includes/functions/TechQueue_Add.php
+++ includes/functions/TechQueue_Add.php
@@ -20,50 +20,44 @@ function TechQueue_Add(&$ThePlanet, &$TheUser, $TechID)
         $QueueLength = 0;
     }
 
-    $MaxLength = (isPro($TheUser) ? MAX_TECH_QUEUE_LENGTH_PRO : MAX_TECH_QUEUE_LENGTH);
     $Modifier = [];
 
-    if($QueueLength + 1 <= $MaxLength)
+    if($QueueLength > 0)
     {
-        if($QueueLength > 0)
+        foreach($Queue as $QueueElement)
         {
-            foreach($Queue as $QueueElement)
+            $QueueElement = explode(',', $QueueElement);
+            if(!isset($Modifier[$_Vars_GameElements[$QueueElement[0]]]))
             {
-                $QueueElement = explode(',', $QueueElement);
-                if(!isset($Modifier[$_Vars_GameElements[$QueueElement[0]]]))
-                {
-                    $Modifier[$_Vars_GameElements[$QueueElement[0]]] = 0;
-                }
-                $Modifier[$_Vars_GameElements[$QueueElement[0]]] += 1;
-                $TheUser[$_Vars_GameElements[$QueueElement[0]]] += 1;
-                $StartTime = $QueueElement[3];
+                $Modifier[$_Vars_GameElements[$QueueElement[0]]] = 0;
             }
+            $Modifier[$_Vars_GameElements[$QueueElement[0]]] += 1;
+            $TheUser[$_Vars_GameElements[$QueueElement[0]]] += 1;
+            $StartTime = $QueueElement[3];
         }
-        else
-        {
-            $StartTime = 0;
-        }
-        if($StartTime <= 0)
-        {
-            $StartTime = time();
-        }
-        $Time = GetBuildingTime($TheUser, $ThePlanet, $TechID);
-        $EndTime = $StartTime + $Time;
-        $NextLevel = $TheUser[$_Vars_GameElements[$TechID]] + 1;
+    }
+    else
+    {
+        $StartTime = 0;
+    }
+    if($StartTime <= 0)
+    {
+        $StartTime = time();
+    }
+    $Time = GetBuildingTime($TheUser, $ThePlanet, $TechID);
+    $EndTime = $StartTime + $Time;
+    $NextLevel = $TheUser[$_Vars_GameElements[$TechID]] + 1;
 
-        $Queue[] = "{$TechID},{$NextLevel},{$Time},{$EndTime}";
+    $Queue[] = "{$TechID},{$NextLevel},{$Time},{$EndTime}";
 
-        $ThePlanet['techQueue'] = implode(';', $Queue);
+    $ThePlanet['techQueue'] = implode(';', $Queue);
 
-        foreach($Modifier as $ElementField => $Value)
-        {
-            $TheUser[$ElementField] -= $Value;
-        }
-
-        return true;
+    foreach($Modifier as $ElementField => $Value)
+    {
+        $TheUser[$ElementField] -= $Value;
     }
 
-    return false;
+    return true;
 }
 
 ?>
