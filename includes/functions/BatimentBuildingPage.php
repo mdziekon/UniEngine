70a591464bbd299bcecc8a1892c03abbe922ca7b
Michal Dziekonski <glibnes@gmail.com>
2020-03-03 23:43:49 +0100

    GH-63 Simplify planet_type check when rendering elements

--- includes/functions/BatimentBuildingPage.php
+++ includes/functions/BatimentBuildingPage.php
@@ -89,107 +89,107 @@ function BatimentBuildingPage(&$CurrentPlanet, $CurrentUser)
         Users\getMaxStructuresQueueLength($CurrentUser)
     );
 
-    foreach($_Vars_ElementCategories['build'] as $Element)
-    {
-        if(in_array($Element, $_Vars_ElementCategories['buildOn'][$CurrentPlanet['planet_type']]))
-        {
-            $elementQueuedLevel = Elements\getElementState($Element, $CurrentPlanet, $CurrentUser)['level'];
-            $isElementInQueue = isset(
-                $queueStateDetails['queuedElementLevelModifiers'][$Element]
-            );
-            $elementQueueLevelModifier = (
-                $isElementInQueue ?
-                $queueStateDetails['queuedElementLevelModifiers'][$Element] :
-                0
-            );
-            $elementCurrentLevel = (
-                $elementQueuedLevel +
-                ($elementQueueLevelModifier * -1)
-            );
-
-            $elementMaxLevel = Elements\getElementMaxUpgradeLevel($Element);
-            $hasReachedMaxLevel = (
-                $elementQueuedLevel >=
-                $elementMaxLevel
-            );
-
-            $hasUpgradeResources = IsElementBuyable($CurrentUser, $CurrentPlanet, $Element, false);
-
-            $hasTechnologyRequirementMet = IsTechnologieAccessible($CurrentUser, $CurrentPlanet, $Element);
-            $isBlockedByTechResearchProgress = (
-                $Element == 31 &&
-                $CurrentUser['techQueue_Planet'] > 0 &&
-                $CurrentUser['techQueue_EndTime'] > 0 &&
-                !isLabUpgradableWhileInUse()
-            );
-
-            $isUpgradePossible = (!$hasReachedMaxLevel);
-            $isUpgradeQueueable = (
-                $isUpgradePossible &&
-                !$isUserOnVacation &&
-                !$isQueueFull &&
-                $hasAvailableFieldsOnPlanet &&
-                $hasTechnologyRequirementMet &&
-                !$isBlockedByTechResearchProgress
-            );
-            $isUpgradeAvailableNow = (
-                $isUpgradeQueueable &&
-                $hasUpgradeResources
-            );
-            $isUpgradeQueueableNow = (
-                $isUpgradeQueueable &&
-                $hasElementsInQueue
-            );
-
-            $blockReason = [];
-            $showInactiveUpgradeActionLink = false;
-
-            if ($hasReachedMaxLevel) {
-                $blockReason[] = $_Lang['ListBox_Disallow_MaxLevelReached'];
-            }
-            if ($isBlockedByTechResearchProgress) {
-                $blockReason[] = $_Lang['ListBox_Disallow_LabResearch'];
-            }
-            if (!$hasAvailableFieldsOnPlanet) {
-                $blockReason[] = $_Lang['ListBox_Disallow_NoFreeFields'];
-            }
-            if ($isQueueFull) {
-                $blockReason[] = $_Lang['ListBox_Disallow_QueueIsFull'];
-            }
-            if ($isUserOnVacation) {
-                $blockReason[] = $_Lang['ListBox_Disallow_VacationMode'];
-            }
-            if (!$hasUpgradeResources) {
-                $showInactiveUpgradeActionLink = true;
-            }
-
-            $listElement = Development\Components\ListViewElementRow\render([
-                'elementID' => $Element,
-                'user' => $CurrentUser,
-                'planet' => $CurrentPlanet,
-                'timestamp' => $Now,
-                'isQueueActive' => $hasElementsInQueue,
-                'elementDetails' => [
-                    'currentState' => $elementCurrentLevel,
-                    'isInQueue' => $isElementInQueue,
-                    'queueLevelModifier' => $elementQueueLevelModifier,
-                    'hasTechnologyRequirementMet' => $hasTechnologyRequirementMet,
-                    'isUpgradeAvailableNow' => $isUpgradeAvailableNow,
-                    'isUpgradeQueueableNow' => $isUpgradeQueueableNow,
-                    'whyUpgradeImpossible' => (
-                        !empty($blockReason) ?
-                        [ end($blockReason) ] :
-                        []
-                    ),
-                ],
-                'getUpgradeElementActionLinkHref' => function () use ($Element) {
-                    return "?cmd=insert&amp;building={$Element}";
-                },
-                'showInactiveUpgradeActionLink' => $showInactiveUpgradeActionLink,
-            ]);
-
-            $BuildingPage .= $listElement['componentHTML'];
+    foreach ($_Vars_ElementCategories['build'] as $Element) {
+        if (!Elements\isStructureAvailableOnPlanetType($Element, $CurrentPlanet['planet_type'])) {
+            continue;
         }
+
+        $elementQueuedLevel = Elements\getElementState($Element, $CurrentPlanet, $CurrentUser)['level'];
+        $isElementInQueue = isset(
+            $queueStateDetails['queuedElementLevelModifiers'][$Element]
+        );
+        $elementQueueLevelModifier = (
+            $isElementInQueue ?
+            $queueStateDetails['queuedElementLevelModifiers'][$Element] :
+            0
+        );
+        $elementCurrentLevel = (
+            $elementQueuedLevel +
+            ($elementQueueLevelModifier * -1)
+        );
+
+        $elementMaxLevel = Elements\getElementMaxUpgradeLevel($Element);
+        $hasReachedMaxLevel = (
+            $elementQueuedLevel >=
+            $elementMaxLevel
+        );
+
+        $hasUpgradeResources = IsElementBuyable($CurrentUser, $CurrentPlanet, $Element, false);
+
+        $hasTechnologyRequirementMet = IsTechnologieAccessible($CurrentUser, $CurrentPlanet, $Element);
+        $isBlockedByTechResearchProgress = (
+            $Element == 31 &&
+            $CurrentUser['techQueue_Planet'] > 0 &&
+            $CurrentUser['techQueue_EndTime'] > 0 &&
+            !isLabUpgradableWhileInUse()
+        );
+
+        $isUpgradePossible = (!$hasReachedMaxLevel);
+        $isUpgradeQueueable = (
+            $isUpgradePossible &&
+            !$isUserOnVacation &&
+            !$isQueueFull &&
+            $hasAvailableFieldsOnPlanet &&
+            $hasTechnologyRequirementMet &&
+            !$isBlockedByTechResearchProgress
+        );
+        $isUpgradeAvailableNow = (
+            $isUpgradeQueueable &&
+            $hasUpgradeResources
+        );
+        $isUpgradeQueueableNow = (
+            $isUpgradeQueueable &&
+            $hasElementsInQueue
+        );
+
+        $blockReason = [];
+        $showInactiveUpgradeActionLink = false;
+
+        if ($hasReachedMaxLevel) {
+            $blockReason[] = $_Lang['ListBox_Disallow_MaxLevelReached'];
+        }
+        if ($isBlockedByTechResearchProgress) {
+            $blockReason[] = $_Lang['ListBox_Disallow_LabResearch'];
+        }
+        if (!$hasAvailableFieldsOnPlanet) {
+            $blockReason[] = $_Lang['ListBox_Disallow_NoFreeFields'];
+        }
+        if ($isQueueFull) {
+            $blockReason[] = $_Lang['ListBox_Disallow_QueueIsFull'];
+        }
+        if ($isUserOnVacation) {
+            $blockReason[] = $_Lang['ListBox_Disallow_VacationMode'];
+        }
+        if (!$hasUpgradeResources) {
+            $showInactiveUpgradeActionLink = true;
+        }
+
+        $listElement = Development\Components\ListViewElementRow\render([
+            'elementID' => $Element,
+            'user' => $CurrentUser,
+            'planet' => $CurrentPlanet,
+            'timestamp' => $Now,
+            'isQueueActive' => $hasElementsInQueue,
+            'elementDetails' => [
+                'currentState' => $elementCurrentLevel,
+                'isInQueue' => $isElementInQueue,
+                'queueLevelModifier' => $elementQueueLevelModifier,
+                'hasTechnologyRequirementMet' => $hasTechnologyRequirementMet,
+                'isUpgradeAvailableNow' => $isUpgradeAvailableNow,
+                'isUpgradeQueueableNow' => $isUpgradeQueueableNow,
+                'whyUpgradeImpossible' => (
+                    !empty($blockReason) ?
+                    [ end($blockReason) ] :
+                    []
+                ),
+            ],
+            'getUpgradeElementActionLinkHref' => function () use ($Element) {
+                return "?cmd=insert&amp;building={$Element}";
+            },
+            'showInactiveUpgradeActionLink' => $showInactiveUpgradeActionLink,
+        ]);
+
+        $BuildingPage .= $listElement['componentHTML'];
     }
 
     foreach ($queueStateDetails['queuedResourcesToUse'] as $resourceKey => $resourceValue) {
