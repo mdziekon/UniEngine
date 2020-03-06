3bd890ff96b157ee0f47659ac69798673cd63a55
Michal Dziekonski <glibnes@gmail.com>
2020-03-06 18:11:47 +0100

    GH-63 Create groupBy utility

--- includes/helpers/common/collections.functions.php
+++ includes/helpers/common/collections.functions.php
@@ -6,4 +6,20 @@ function firstN ($collection, $elementsCount) {
     return array_slice($collection, 0, $elementsCount);
 }
 
+function groupBy($collection, $iteratee) {
+    $groupedCollection = [];
+
+    foreach ($collection as $key => $value) {
+        $groupKey = $iteratee($value, $key);
+
+        if (!isset($groupedCollection[$groupKey])) {
+            $groupedCollection[$groupKey] = [];
+        }
+
+        $groupedCollection[$groupKey][] = $value;
+    }
+
+    return $groupedCollection;
+}
+
 ?>
