c107f6e4edba51cdda5b83ad7a836b93944060f1
Michal Dziekonski <glibnes@gmail.com>
2020-03-29 18:51:08 +0200

    GH-91 Implement generic resources loss calculator

--- modules/flights/_includes.php
+++ modules/flights/_includes.php
@@ -6,6 +6,7 @@ call_user_func(function () {
 
     $includePath = $_EnginePath . 'modules/flights/';
 
+    include($includePath . './utils/calculations/calculateResourcesLoss.utils.php');
     include($includePath . './utils/missions.utils.php');
 
 });
