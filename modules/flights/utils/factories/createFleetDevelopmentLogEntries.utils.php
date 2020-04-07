ec7beebe6d746dfde965f3004b7e73b34ea6c497
Michal Dziekonski <glibnes@gmail.com>
2020-04-08 00:32:17 +0200

    GH-91 Support resourcesPillage in createFleetDevelopmentLogEntries()

--- modules/flights/utils/factories/createFleetDevelopmentLogEntries.utils.php
+++ modules/flights/utils/factories/createFleetDevelopmentLogEntries.utils.php
@@ -6,12 +6,19 @@ namespace UniEngine\Engine\Modules\Flights\Utils\Factories;
  * @param array $params
  * @param array $params['originalShips'] Ships that have participated in the combat
  * @param array $params['postCombatShips'] Ships that have survived the combat
+ * @param array $params['resourcesPillage']
+ *              (default: `[]`)
  */
 function createFleetDevelopmentLogEntries($params) {
     $originalShips = $params['originalShips'];
     $postCombatShips = (
         isset($params['postCombatShips']) ?
             $params['postCombatShips'] :
+            []
+    );
+    $resourcesPillage = (
+        isset($params['resourcesPillage']) ?
+            $params['resourcesPillage'] :
             []
     );
 
@@ -30,6 +37,29 @@ function createFleetDevelopmentLogEntries($params) {
         }
 
         $entries[] = implode(',', [ $shipID, $shipCountDifference ]);
+    }
+
+    $pillagableResourceKeyMapping = [
+        'metal' => 'M',
+        'crystal' => 'C',
+        'deuterium' => 'D',
+    ];
+
+    foreach ($resourcesPillage as $resourceKey => $pillagedAmount) {
+        if ($pillagedAmount <= 0) {
+            continue;
+        }
+        if (!isset($pillagableResourceKeyMapping[$resourceKey])) {
+            // No mapping available yet,
+            // probably because of a custom resource added to the game
+            continue;
+        }
+
+        $newEntryParams = [
+            $pillagableResourceKeyMapping[$resourceKey],
+            $pillagedAmount,
+        ];
+        $entries[] = implode(',', $newEntryParams);
     }
 
     return $entries;
