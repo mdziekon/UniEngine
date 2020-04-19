67249b5212b4b67a235f83bd05574953291eb237
Michal Dziekonski <glibnes@gmail.com>
2020-04-19 05:32:21 +0200

    GH-91 Unify morale cache accesses some more

--- modules/flights/utils/fleetCache/morale.utils.php
+++ modules/flights/utils/fleetCache/morale.utils.php
@@ -6,6 +6,17 @@ namespace UniEngine\Engine\Modules\Flights\Utils\FleetCache;
  * @param array $params
  * @param ref $params['fleetCache']
  * @param array $params['userID']
+ */
+function hasCachedMoraleData($params) {
+    $userID = $params['userID'];
+
+    return !empty($params['fleetCache']['MoraleCache'][$userID]);
+}
+
+/**
+ * @param array $params
+ * @param ref $params['fleetCache']
+ * @param array $params['userID']
  * @param array $params['userMoraleData']
  */
 function updateMoraleDataCache($params) {
@@ -18,6 +29,26 @@ function updateMoraleDataCache($params) {
         'droptime' => $userMoraleData['morale_droptime'],
         'lastupdate' => $userMoraleData['morale_lastupdate'],
     ];
+}
+
+/**
+ * @param array $params
+ * @param ref $params['destination']
+ * @param ref $params['fleetCache']
+ * @param array $params['userID']
+ */
+function loadMoraleDataFromCache($params) {
+    $destination = &$params['destination'];
+    $userID = $params['userID'];
+    $cacheEntry = $params['fleetCache']['MoraleCache'][$userID];
+
+    if (!hasCachedMoraleData($params)) {
+        return;
+    }
+
+    $destination['morale_level'] = $cacheEntry['level'];
+    $destination['morale_droptime'] = $cacheEntry['droptime'];
+    $destination['morale_lastupdate'] = $cacheEntry['lastupdate'];
 }
 
 ?>
