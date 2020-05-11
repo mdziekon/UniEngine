54be240cc0d46ab6d5ea09ec6febb13a00686e9f
Michal Dziekonski <glibnes@gmail.com>
2020-05-11 21:16:47 +0200

    GH-91 Remove unused "userData" property from the result

--- modules/flights/utils/initializers/defenderDetails.utils.php
+++ modules/flights/utils/initializers/defenderDetails.utils.php
@@ -21,16 +21,6 @@ function initDefenderDetails($params) {
     $combatTechnologies = Flights\Utils\Initializers\initCombatTechnologiesMap([
         'user' => $fleetData,
     ]);
-    $userData = [
-        'id' => $fleetOwnerID,
-        'username' => $fleetData['username'],
-        'techs' => Array2String($combatTechnologies),
-        'pos' => _createFleetCoordinatesString($fleetData),
-    ];
-
-    if (!empty($fleetData['ally_tag'])) {
-        $userData['ally'] = $fleetData['ally_tag'];
-    }
 
     if (MORALE_ENABLED) {
         if (empty($localCache['MoraleCache'][$fleetOwnerID])) {
@@ -40,8 +30,6 @@ function initDefenderDetails($params) {
                 $fleetData['morale_lastupdate'] = $fleetCache['MoraleCache'][$fleetOwnerID]['lastupdate'];
             }
             Morale_ReCalculate($fleetData, $combatTimestamp);
-            $userData['morale'] = $fleetData['morale_level'];
-            $userData['moralePoints'] = $fleetData['morale_points'];
 
             $localCache['MoraleCache'][$fleetOwnerID] = [
                 'level' => $fleetData['morale_level'],
@@ -49,13 +37,10 @@ function initDefenderDetails($params) {
                 'droptime' => $fleetData['morale_droptime'],
                 'lastupdate' => $fleetData['morale_lastupdate'],
             ];
-        } else {
-            $userData['morale'] = $localCache['MoraleCache'][$fleetOwnerID]['level'];
-            $userData['moralePoints'] = $localCache['MoraleCache'][$fleetOwnerID]['points'];
         }
 
         $moraleCombatModifiers = Flights\Utils\Modifiers\calculateMoraleCombatModifiers([
-            'moraleLevel' => $userData['morale'],
+            'moraleLevel' => $localCache['MoraleCache'][$fleetOwnerID]['level'],
         ]);
 
         $combatTechnologies = array_merge(
@@ -69,12 +54,7 @@ function initDefenderDetails($params) {
         'fleetOwnerID' => $fleetOwnerID,
         'ships' => String2Array($fleetData['fleet_array']),
         'combatTechnologies' => $combatTechnologies,
-        'userData' => $userData,
     ];
-}
-
-function _createFleetCoordinatesString($fleetData) {
-    return "{$fleetData['fleet_start_galaxy']}:{$fleetData['fleet_start_system']}:{$fleetData['fleet_start_planet']}";
 }
 
 ?>
