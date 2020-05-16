3977d86f2b2dee9acded43ead8afb8d75453fccc
Michal Dziekonski <glibnes@gmail.com>
2020-05-16 15:11:32 +0200

    GH-91 Create an util allowing to roll moon creation

--- modules/flights/_includes.php
+++ modules/flights/_includes.php
@@ -6,6 +6,7 @@ call_user_func(function () {
 
     $includePath = $_EnginePath . 'modules/flights/';
 
+    include($includePath . './utils/calculations/calculateMoonCreationRoll.utils.php');
     include($includePath . './utils/calculations/calculatePostCombatMorale.utils.php');
     include($includePath . './utils/calculations/calculatePostCombatMoraleUpdates.utils.php');
     include($includePath . './utils/calculations/calculatePillageFactor.utils.php');
