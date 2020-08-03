07e262a8728a5e2cff9e78e1542d88b968f0401f
Michal Dziekonski <glibnes@gmail.com>
2020-08-03 02:24:32 +0200

    GH-90 Move duplicated batch deletion code into a separate module unit

--- modules/messages/commands/batchDeleteMessagesByID.commands.php
+++ modules/messages/commands/batchDeleteMessagesByID.commands.php
@@ -15,115 +15,13 @@ use UniEngine\Engine\Modules\Messages;
  *               The ID of a user whose messages should be deleted.
  */
 function batchDeleteMessagesByID($params) {
-    $messageIDs = $params['messageIDs'];
-    $ownerID = $params['userID'];
-
-    $excludedMessageTypesString = Messages\Utils\getBatchActionsExcludedMessageTypesQueryString();
-    $messageIDsString = implode(', ', $messageIDs);
-
-    $softDeleteMessagesQuery = (
-        "UPDATE {{table}} " .
-        "SET " .
-        "`deleted` = true, " .
-        "`Thread_IsLast` = 0 " .
-        "WHERE " .
-        "`type` NOT IN ({$excludedMessageTypesString}) AND " .
-        "`id` IN ({$messageIDsString}) AND " .
-        "`id_owner` = {$ownerID} " .
-        ";"
-    );
-
-    doquery($softDeleteMessagesQuery, 'messages');
-
-    $deletedMessagesCount = getDBLink()->affected_rows;
-
-    _updateMessageThreadsAffectedByBatchDeletionByID($params);
-
-    return [
-        'deletedMessagesCount' => $deletedMessagesCount,
-    ];
-}
-
-/**
- * @param array $params
- *              See batchDeleteUserMessages().
- */
-function _updateMessageThreadsAffectedByBatchDeletionByID($params) {
-    $messageIDs = $params['messageIDs'];
-    $ownerID = $params['userID'];
-
-    $excludedMessageTypesString = Messages\Utils\getBatchActionsExcludedMessageTypesQueryString();
-    $messageIDsString = implode(', ', $messageIDs);
-
-    $fetchThreadedMessagesQuery = (
-        "SELECT `Thread_ID` FROM {{table}} " .
-        "WHERE " .
-        "`type` NOT IN ({$excludedMessageTypesString}) AND " .
-        "`id` IN ({$messageIDsString}) AND " .
-        "`id_owner` = {$ownerID} AND " .
-        "`Thread_ID` > 0 " .
-        ";"
-    );
-
-    $threadedMessagesResult = doquery($fetchThreadedMessagesQuery, 'messages');
-
-    if ($threadedMessagesResult->num_rows <= 0) {
-        return;
-    }
-
-    $threadsToUpdate = [];
-
-    while ($threadData = $threadedMessagesResult->fetch_assoc()) {
-        $threadID = $threadData['Thread_ID'];
-
-        if (in_array($threadID, $threadsToUpdate)) {
-            continue;
-        }
-
-        $threadsToUpdate[] = $threadID;
-    }
-
-    $threadsToUpdateString = implode(', ', $threadsToUpdate);
-
-    $fetchLastNonDeletedMessageIDsOfThreadsQuery = (
-        "SELECT MAX(`id`) AS `id` " .
-        "FROM {{table}} " .
-        "WHERE " .
-        "`Thread_ID` IN ({$threadsToUpdateString}) AND " .
-        "`id_owner` = {$ownerID} AND " .
-        "`deleted` = false " .
-        "GROUP BY `Thread_ID` " .
-        ";"
-    );
-    $fetchLastNonDeletedMessageIDsOfThreadsResult = doquery(
-        $fetchLastNonDeletedMessageIDsOfThreadsQuery,
-        'messages'
-    );
-
-    if ($fetchLastNonDeletedMessageIDsOfThreadsResult->num_rows <= 0) {
-        return;
-    }
-
-    $threadMessagesToUpdate = [];
-
-    while ($messageEntry = $fetchLastNonDeletedMessageIDsOfThreadsResult->fetch_assoc()) {
-        $messageID = $messageEntry['id'];
-
-        $threadMessagesToUpdate[] = $messageID;
-    }
-
-    $threadMessagesToUpdateString = implode(', ', $threadMessagesToUpdate);
-
-    $updateThreadedMessagesQuery = (
-        "UPDATE {{table}} " .
-        "SET " .
-        "`Thread_IsLast` = 1 " .
-        "WHERE " .
-        "`id` IN ({$threadMessagesToUpdateString}) " .
-        ";"
-    );
-
-    doquery($updateThreadedMessagesQuery, 'messages');
+    return Messages\Utils\batchDeleteMessages([
+        'criteria' => [
+            'userID' => $params['userID'],
+            'messageIDs' => $params['messageIDs'],
+            'excludeTypeIDs' => Messages\Utils\getBatchActionsExcludedMessageTypes(),
+        ],
+    ]);
 }
 
 ?>
--- modules/messages/commands/batchDeleteMessagesOlderThan.commands.php
+++ modules/messages/commands/batchDeleteMessagesOlderThan.commands.php
@@ -18,130 +18,22 @@ use UniEngine\Engine\Modules\Messages;
  *               prior to this point in time (inclusive) will be deleted.
  */
 function batchDeleteMessagesOlderThan($params) {
-    $ownerID = $params['userID'];
-    $untilTimestamp = $params['untilTimestamp'];
-    $messageTypeID = (
-        isset($params['messageTypeID']) ?
-        $params['messageTypeID'] :
-        null
-    );
-
-    $excludedMessageTypesString = Messages\Utils\getBatchActionsExcludedMessageTypesQueryString();
-
-    $softDeleteMessagesQuery = (
-        "UPDATE {{table}} " .
-        "SET " .
-        "`deleted` = true " .
-        "WHERE " .
-        (
-            $messageTypeID !== null ?
-            "`type` = {$messageTypeID} AND " :
-            "`type` NOT IN ({$excludedMessageTypesString}) AND "
-        ) .
-        "`time` <= {$untilTimestamp} AND " .
-        "`id_owner` = {$ownerID} " .
-        ";"
-    );
-
-    doquery($softDeleteMessagesQuery, 'messages');
-
-    $deletedMessagesCount = getDBLink()->affected_rows;
-
-    _updateMessageThreadsAffectedByBatchDeletion($params);
-
-    return [
-        'deletedMessagesCount' => $deletedMessagesCount,
-    ];
-}
-
-/**
- * @param array $params
- *              See batchDeleteUserMessages().
- */
-function _updateMessageThreadsAffectedByBatchDeletion($params) {
-    $ownerID = $params['userID'];
-    $untilTimestamp = $params['untilTimestamp'];
-    $messageTypeID = (
-        isset($params['messageTypeID']) ?
-        $params['messageTypeID'] :
-        null
-    );
-
-    $excludedMessageTypesString = Messages\Utils\getBatchActionsExcludedMessageTypesQueryString();
-
-    $fetchThreadedMessagesQuery = (
-        "SELECT `Thread_ID` FROM {{table}} " .
-        "WHERE " .
-        (
-            $messageTypeID !== null ?
-            "`type` = {$messageTypeID} AND " :
-            "`type` NOT IN ({$excludedMessageTypesString}) AND "
-        ) .
-        "`time` <= {$untilTimestamp} AND " .
-        "`id_owner` = {$ownerID} AND " .
-        "`Thread_ID` > 0 " .
-        ";"
-    );
-
-    $threadedMessagesResult = doquery($fetchThreadedMessagesQuery, 'messages');
-
-    if ($threadedMessagesResult->num_rows <= 0) {
-        return;
-    }
-
-    $threadsToUpdate = [];
-
-    while ($threadData = $threadedMessagesResult->fetch_assoc()) {
-        $threadID = $threadData['Thread_ID'];
-
-        if (in_array($threadID, $threadsToUpdate)) {
-            continue;
-        }
-
-        $threadsToUpdate[] = $threadID;
-    }
-
-    $threadsToUpdateString = implode(', ', $threadsToUpdate);
-
-    $fetchLastNonDeletedMessageIDsOfThreadsQuery = (
-        "SELECT MAX(`id`) AS `id` " .
-        "FROM {{table}} " .
-        "WHERE " .
-        "`Thread_ID` IN ({$threadsToUpdateString}) AND " .
-        "`id_owner` = {$ownerID} AND " .
-        "`deleted` = false " .
-        "GROUP BY `Thread_ID` " .
-        ";"
-    );
-    $fetchLastNonDeletedMessageIDsOfThreadsResult = doquery(
-        $fetchLastNonDeletedMessageIDsOfThreadsQuery,
-        'messages'
-    );
-
-    if ($fetchLastNonDeletedMessageIDsOfThreadsResult->num_rows <= 0) {
-        return;
-    }
-
-    $threadMessagesToUpdate = [];
-
-    while ($messageEntry = $fetchLastNonDeletedMessageIDsOfThreadsResult->fetch_assoc()) {
-        $messageID = $messageEntry['id'];
-
-        $threadMessagesToUpdate[] = $messageID;
-    }
-
-    $threadMessagesToUpdateString = implode(', ', $threadMessagesToUpdate);
-
-    $updateThreadedMessagesQuery = (
-        "UPDATE {{table}} " .
-        "SET " .
-        "`Thread_IsLast` = 1 " .
-        "WHERE " .
-        "`id` IN ({$threadMessagesToUpdateString}) " .
-        ";"
-    );
-
-    doquery($updateThreadedMessagesQuery, 'messages');
+    return Messages\Utils\batchDeleteMessages([
+        'criteria' => [
+            'userID' => $params['userID'],
+            'untilTimestamp' => $params['untilTimestamp'],
+            'includeTypeIDs' => (
+                isset($params['messageTypeID']) ?
+                [ $params['messageTypeID'] ] :
+                []
+            ),
+            'excludeTypeIDs' => (
+                !isset($params['messageTypeID']) ?
+                Messages\Utils\getBatchActionsExcludedMessageTypes() :
+                []
+            ),
+        ],
+    ]);
 }
 
 ?>
