d4865e2eb0ca0bb3e7d4fb63acfc0cfa050df152
Michal Dziekonski <glibnes@gmail.com>
2020-12-28 02:15:44 +0100

    GH-139 Initial implementation of the fleets blockade system entries fetcher

---
+++ modules/flightControl/utils/fetchers/fetchActiveSmartFleetsBlockadeEntries.fetcher.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace UniEngine\Engine\Modules\FlightControl\Utils\Fetchers;
+
+use UniEngine\Engine\Includes\Helpers\Common\Collections;
+
+/**
+ * @param array $props
+ * @param number $props['timestamp']
+ * @param array $props['fleetOwnerDetails']
+ * @param number $props['fleetOwnerDetails']['userId']
+ * @param number $props['fleetOwnerDetails']['planetId']
+ * @param array $props['targetOwnerDetails']
+ * @param number $props['targetOwnerDetails']['userId']
+ * @param number $props['targetOwnerDetails']['planetId']
+ */
+function fetchActiveSmartFleetsBlockadeEntries ($props) {
+    $timestamp = $props['timestamp'];
+    $targetIsOccupied = !empty($props['targetOwnerDetails']);
+
+    $userIdsToCheck = Collections\compact([
+        $props['fleetOwnerDetails']['userId'],
+        (
+            $targetIsOccupied ?
+            $props['targetOwnerDetails']['userId'] :
+            null
+        ),
+    ]);
+    $planetIdsToCheck = Collections\compact([
+        $props['fleetOwnerDetails']['planetId'],
+        (
+            $targetIsOccupied ?
+            $props['targetOwnerDetails']['planetId'] :
+            null
+        ),
+    ]);
+
+    $query = (
+        "SELECT " .
+        "`Type`, `BlockMissions`, `Reason`, `StartTime`, `EndTime`, `PostEndTime`, `ElementID`, `DontBlockIfIdle` " .
+        "FROM {{table}} " .
+        "WHERE " .
+        "`StartTime` <= {$timestamp} AND " .
+        "( " .
+            "(`Type` = 1 AND (`EndTime` > {$timestamp} OR `PostEndTime` > {$timestamp})) OR " .
+            "(`Type` = 2 AND `ElementID` IN ({$userIdsToCheck}) AND `EndTime` > {$timestamp}) OR " .
+            "(`Type` = 3 AND `ElementID` IN ({$planetIdsToCheck}) AND `EndTime` > {$timestamp}) OR " .
+        ") " .
+        "ORDER BY `Type` ASC, `EndTime` DESC " .
+        ";"
+    );
+
+    return doquery($query, 'smart_fleet_blockade');
+}
+
+?>
