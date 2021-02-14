30558522724d91c65e2c51ee4e8e7b924edd47d6
Michal Dziekonski <glibnes@gmail.com>
2021-02-14 23:40:44 +0100

    GH-150 Move input handling to LoginView's code

--- login.php
+++ login.php
@@ -6,98 +6,10 @@ define('UEC_INLOGIN', true);
 $_DontShowMenus = true;
 
 $_EnginePath = './';
-include($_EnginePath.'common.php');
+include($_EnginePath . 'common.php');
 include_once($_EnginePath . 'modules/session/_includes.php');
 
-use UniEngine\Engine\Includes\Helpers\Users;
 use UniEngine\Engine\Modules\Session;
-
-includeLang('login');
-
-$loginAttemptResult = null;
-
-if ($_POST) {
-    // TODO: Remove this useless block
-} else if (Session\Utils\Cookie\hasSessionCookie()) {
-    $loginAttemptResult = Session\Input\CookieLogin\handleCookieLogin([]);
-
-    if ($loginAttemptResult['isSuccess']) {
-        Session\Utils\Redirects\redirectToOverview();
-
-        die();
-    }
-}
-
-if ($_POST) {
-    include_once($_EnginePath . '/includes/functions/IPandUA_Logger.php');
-
-    $ipHash = md5(Users\Session\getCurrentIP());
-
-    $loginAttemptResult = Session\Input\LocalIdentityLogin\handleLocalIdentityLogin([
-        'input' => &$_POST,
-        'ipHash' => $ipHash,
-        'currentTimestamp' => time(),
-    ]);
-
-    if ($loginAttemptResult['isSuccess']) {
-        $userEntity = $loginAttemptResult['payload']['userEntity'];
-
-        IPandUA_Logger($userEntity, false);
-
-        Session\Utils\Redirects\redirectToOverview();
-
-        die();
-    }
-
-    Session\Utils\RateLimiter\updateLoginRateLimiterEntry([
-        'ipHash' => $ipHash,
-    ]);
-
-    if (isset($loginAttemptResult['error']['userEntity'])) {
-        $userEntity = $loginAttemptResult['error']['userEntity'];
-
-        IPandUA_Logger($userEntity, true);
-    }
-}
-
-if (
-    $loginAttemptResult &&
-    !$loginAttemptResult['isSuccess']
-) {
-    $errorCode = $loginAttemptResult['error']['code'];
-
-    switch ($errorCode) {
-        case 'NO_COOKIE':
-        case 'INVALID_USER_ID':
-        case 'USER_NOT_FOUND':
-        case 'INVALID_PASSWORD':
-            $errorMessage = $_Lang['Login_InvalidCredentials'];
-            break;
-        case 'INVALID_UNIVERSUM_CODE':
-            $errorMessage = $_Lang['Login_BadUniversum'];
-            break;
-        case 'INVALID_USERNAME_CHARACTERS':
-            $errorMessage = $_Lang['Login_BadSignsUser'];
-            break;
-        case 'UNIVERSUM_NOT_OPEN_YET':
-            $serverOpeningTimestamp = $loginAttemptResult['error']['openingTimestamp'];
-
-            $errorMessage = sprintf(
-                $_Lang['Login_UniversumNotStarted'],
-                prettyDate('d m Y', $serverOpeningTimestamp, 1),
-                date('H:i:s', $serverOpeningTimestamp)
-            );
-            break;
-        case 'LOGIN_ATTEMPTS_RATE_LIMITED':
-            $errorMessage = $_Lang['Login_FailLoginProtection'];
-            break;
-        default:
-            $errorMessage = $_Lang['Login_UnknownError'];
-            break;
-    }
-
-    message($errorMessage, $_Lang['Err_Title']);
-}
 
 if (!LOGINPAGE_ALLOW_LOGINPHP) {
     Session\Utils\Redirects\permaRedirectToMainDomain();
@@ -105,21 +17,11 @@ if (!LOGINPAGE_ALLOW_LOGINPHP) {
     die();
 }
 
-if ($_GET && !$_POST) {
-    $langChangeAttemptResult = Session\Input\Language\handleLanguageChange([
-        'input' => &$_GET,
-        'currentTimestamp' => time(),
-    ]);
+$pageView = Session\Screens\LoginView\render([
+    'currentTimestamp' => time(),
+]);
 
-    if (
-        $langChangeAttemptResult['isSuccess'] &&
-        $langChangeAttemptResult['payload']['hasLangChanged']
-    ) {
-        includeLang('login');
-    }
-}
-
-$pageView = Session\Screens\LoginView\render([]);
+includeLang('login');
 
 display($pageView['componentHTML'], $_Lang['Page_Title']);
 
--- modules/session/screens/LoginView/LoginView.component.php
+++ modules/session/screens/LoginView/LoginView.component.php
@@ -2,25 +2,117 @@
 
 namespace UniEngine\Engine\Modules\Session\Screens\LoginView;
 
+use UniEngine\Engine\Modules\Session;
+use UniEngine\Engine\Includes\Helpers\Users;
+
 //  Arguments
 //      - $props (Object)
+//          - currentTimestamp (number)
 //
 //  Returns: Object
 //      - componentHTML (String)
 //
 function render ($props) {
+    global $_EnginePath, $_Lang;
+
     includeLang('login');
 
+    $currentTimestamp = $props['currentTimestamp'];
+
     // Handle user input
-    // $cmdResult = Session\Input\UserCommands\handleCommands(
-    //     $input,
-    //     []
-    // );
-    // if ($cmdResult['isSuccess']) {
-    //     // TODO: do something
-    // } else {
-    //     // TODO: collect errors to display
-    // }
+    $loginAttemptResult = null;
+
+    if ($_GET && !$_POST) {
+        $langChangeAttemptResult = Session\Input\Language\handleLanguageChange([
+            'input' => &$_GET,
+            'currentTimestamp' => $currentTimestamp,
+        ]);
+
+        if (
+            $langChangeAttemptResult['isSuccess'] &&
+            $langChangeAttemptResult['payload']['hasLangChanged']
+        ) {
+            includeLang('login');
+        }
+    } else if ($_POST) {
+        include_once($_EnginePath . '/includes/functions/IPandUA_Logger.php');
+
+        $ipHash = md5(Users\Session\getCurrentIP());
+
+        $loginAttemptResult = Session\Input\LocalIdentityLogin\handleLocalIdentityLogin([
+            'input' => &$_POST,
+            'ipHash' => $ipHash,
+            'currentTimestamp' => $currentTimestamp,
+        ]);
+
+        if ($loginAttemptResult['isSuccess']) {
+            $userEntity = $loginAttemptResult['payload']['userEntity'];
+
+            IPandUA_Logger($userEntity, false);
+
+            Session\Utils\Redirects\redirectToOverview();
+
+            die();
+        }
+
+        Session\Utils\RateLimiter\updateLoginRateLimiterEntry([
+            'ipHash' => $ipHash,
+        ]);
+
+        if (isset($loginAttemptResult['error']['userEntity'])) {
+            $userEntity = $loginAttemptResult['error']['userEntity'];
+
+            IPandUA_Logger($userEntity, true);
+        }
+    } else if (Session\Utils\Cookie\hasSessionCookie()) {
+        $loginAttemptResult = Session\Input\CookieLogin\handleCookieLogin([]);
+
+        if ($loginAttemptResult['isSuccess']) {
+            Session\Utils\Redirects\redirectToOverview();
+
+            die();
+        }
+    }
+
+    // Handle input errors
+    if (
+        $loginAttemptResult &&
+        !$loginAttemptResult['isSuccess']
+    ) {
+        $errorCode = $loginAttemptResult['error']['code'];
+
+        switch ($errorCode) {
+            case 'NO_COOKIE':
+            case 'INVALID_USER_ID':
+            case 'USER_NOT_FOUND':
+            case 'INVALID_PASSWORD':
+                $errorMessage = $_Lang['Login_InvalidCredentials'];
+                break;
+            case 'INVALID_UNIVERSUM_CODE':
+                $errorMessage = $_Lang['Login_BadUniversum'];
+                break;
+            case 'INVALID_USERNAME_CHARACTERS':
+                $errorMessage = $_Lang['Login_BadSignsUser'];
+                break;
+            case 'UNIVERSUM_NOT_OPEN_YET':
+                $serverOpeningTimestamp = $loginAttemptResult['error']['openingTimestamp'];
+
+                $errorMessage = sprintf(
+                    $_Lang['Login_UniversumNotStarted'],
+                    prettyDate('d m Y', $serverOpeningTimestamp, 1),
+                    date('H:i:s', $serverOpeningTimestamp)
+                );
+                break;
+            case 'LOGIN_ATTEMPTS_RATE_LIMITED':
+                $errorMessage = $_Lang['Login_FailLoginProtection'];
+                break;
+            default:
+                $errorMessage = $_Lang['Login_UnknownError'];
+                break;
+        }
+
+        message($errorMessage, $_Lang['Err_Title']);
+    }
 
     $viewProps = [];
     $viewComponent = Components\LoginForm\render($viewProps);
