1c504a935d384c5824cb555ae0dbcd1318456ba0
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-03-18 00:38:18 +0100

    GH-150 Add some documentation regarding data sanitization

--- modules/registration/utils/cookies.utils.php
+++ modules/registration/utils/cookies.utils.php
@@ -2,6 +2,9 @@
 
 namespace UniEngine\Engine\Modules\Registration\Utils\Cookies;
 
+/**
+ * @return float | null Sanitized user Id or `null`
+ */
 function getStoredReferrerId() {
     if (empty($_COOKIE[REFERING_COOKIENAME])) {
         return null;
