a773a2c453de56a147993a4d52afe70223545164
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-03-31 01:41:58 +0200

    GH-91 Split getFleetParams into separate modes

--- modules/flightControl/utils/helpers/getFlightParams.helper.php
+++ modules/flightControl/utils/helpers/getFlightParams.helper.php
@@ -2,43 +2,33 @@
 
 namespace UniEngine\Engine\Modules\FlightControl\Utils\Helpers;
 
-/**
- * @param array $props
- * @param array $props['user']
- * @param array $props['fleet']
- * @param array $props['fleetSpeed']
- * @param array $props['distance']
- * @param array $props['maxFleetSpeed']
- * @param array $props['isUsingQuantumGate']
- * @param array $props['quantumGateUseType']
- */
-function getFleetParams ($params) {
-    if (!$params['isUsingQuantumGate']) {
-        $durationToDestination = getFlightDuration([
-            'speedFactor' => $params['fleetSpeed'],
+function _getFleetParamsNormalFlight($params) {
+    $durationToDestination = getFlightDuration([
+        'speedFactor' => $params['fleetSpeed'],
+        'distance' => $params['distance'],
+        'maxShipsSpeed' => $params['maxFleetSpeed']
+    ]);
+    $durationBackToOrigin = $durationToDestination;
+
+    $fuelConsumption = getFlightTotalConsumption(
+        [
+            'ships' => $params['fleet']['array'],
             'distance' => $params['distance'],
-            'maxShipsSpeed' => $params['maxFleetSpeed']
-        ]);
-        $durationBackToOrigin = $durationToDestination;
+            'duration' => $durationToDestination,
+        ],
+        $params['user']
+    );
 
-        $fuelConsumption = getFlightTotalConsumption(
-            [
-                'ships' => $params['fleet']['array'],
-                'distance' => $params['distance'],
-                'duration' => $durationToDestination,
-            ],
-            $params['user']
-        );
+    return [
+        'duration' => [
+            'toDestination' => $durationToDestination,
+            'backToOrigin' => $durationBackToOrigin,
+        ],
+        'consumption' => $fuelConsumption,
+    ];
+}
 
-        return [
-            'duration' => [
-                'toDestination' => $durationToDestination,
-                'backToOrigin' => $durationBackToOrigin,
-            ],
-            'consumption' => $fuelConsumption,
-        ];
-    }
-
+function _getFleetParamsUsingQuantumGate($params) {
     if ($params['quantumGateUseType'] == 1) {
         return [
             'duration' => [
@@ -73,6 +63,24 @@ function getFleetParams ($params) {
         ],
         'consumption' => $fuelConsumption,
     ];
+}
+
+/**
+ * @param array $props
+ * @param array $props['user']
+ * @param array $props['fleet']
+ * @param array $props['fleetSpeed']
+ * @param array $props['distance']
+ * @param array $props['maxFleetSpeed']
+ * @param array $props['isUsingQuantumGate']
+ * @param array $props['quantumGateUseType']
+ */
+function getFleetParams($params) {
+    return (
+        $params['isUsingQuantumGate'] ?
+            _getFleetParamsUsingQuantumGate($params) :
+            _getFleetParamsNormalFlight($params)
+    );
 }
 
 ?>
