0da81d4b3e59dbf4d2a0c4ff7820beb425993265
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-04-17 03:21:17 +0200

    GH-139 Fix mock data & add multi acs testcase

--- modules/flightControl/components/FlightsList/utils/buildOwnListElement.utils.UETest.php
+++ modules/flightControl/components/FlightsList/utils/buildOwnListElement.utils.UETest.php
@@ -48,7 +48,7 @@ class BuildOwnListElementTestCase extends TestCase {
                 "fleet_mess" => "0",
                 "fleet_mission" => "1",
                 "fleet_array" => "202,100;203,10",
-                "fleet_amount" => "100",
+                "fleet_amount" => "110",
                 "fleet_start_time" => "1591971305",
                 "fleet_end_time" => "1591975805",
                 "fleet_end_stay" => "0",
@@ -80,7 +80,7 @@ class BuildOwnListElementTestCase extends TestCase {
             'FleetMission'              => "Attack",
             'FleetBehaviour'            => 'In flight to the destination point',
             'FleetBehaviourTxt'         => '(In flight)',
-            'FleetCount'                => '100',
+            'FleetCount'                => '110',
             'FleetOriGalaxy'            => '1',
             'FleetOriSystem'            => '1',
             'FleetOriPlanet'            => '1',
@@ -121,7 +121,7 @@ class BuildOwnListElementTestCase extends TestCase {
                 "fleet_mess" => "0",
                 "fleet_mission" => "1",
                 "fleet_array" => "202,100;203,10",
-                "fleet_amount" => "100",
+                "fleet_amount" => "110",
                 "fleet_start_time" => "1591971305",
                 "fleet_end_time" => "1591975805",
                 "fleet_end_stay" => "0",
@@ -172,7 +172,7 @@ class BuildOwnListElementTestCase extends TestCase {
             'FleetMission'              => "United Attack #1",
             'FleetBehaviour'            => 'In flight to the destination point',
             'FleetBehaviourTxt'         => '(In flight)',
-            'FleetCount'                => '101',
+            'FleetCount'                => '111',
             'FleetOriGalaxy'            => '1',
             'FleetOriSystem'            => '1',
             'FleetOriPlanet'            => '1',
@@ -197,6 +197,123 @@ class BuildOwnListElementTestCase extends TestCase {
             ],
             'extraShipsInUnion' => [
                 "202" => 1,
+            ],
+        ];
+
+        $this->assertArraySubset($expectedParams, $result, true);
+        $this->assertArraySubset($expectedParamsData, $result['data'], true);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldCreateCorrectElementWithMultiAcsStructure() {
+        $params = [
+            "elementNo" => 1,
+            "fleetEntry" => [
+                "fleet_id" => "1269",
+                "fleet_mess" => "0",
+                "fleet_mission" => "1",
+                "fleet_array" => "202,100;203,10",
+                "fleet_amount" => "110",
+                "fleet_start_time" => "1591971305",
+                "fleet_end_time" => "1591975805",
+                "fleet_end_stay" => "0",
+                "fleet_send_time" => "1591966805",
+                "fleet_start_galaxy" => "1",
+                "fleet_start_system" => "1",
+                "fleet_start_planet" => "1",
+                "fleet_start_type" => "1",
+                "fleet_end_galaxy" => "2",
+                "fleet_end_system" => "5",
+                "fleet_end_planet" => "10",
+                "fleet_end_type" => "3",
+                "fleet_resource_metal" => "0",
+                "fleet_resource_crystal" => "0",
+                "fleet_resource_deuterium" => "0",
+            ],
+            "acsMainFleets" => [
+                "1269" => [
+                    "acsId" => "113",
+                    "hasJoinedFleets" => true,
+                ],
+            ],
+            "currentTimestamp" => 1591968605,
+            "acsUnionsExtraSquads" => [
+                "1269" => [
+                    [
+                        "array" => [
+                            "202" => "2",
+                        ],
+                        "count" => "2",
+                    ],
+                    [
+                        "array" => [
+                            "204" => "5",
+                        ],
+                        "count" => "5",
+                    ],
+                    [
+                        "array" => [
+                            "202" => "1",
+                            "203" => "1",
+                        ],
+                        "count" => "2",
+                    ],
+                ],
+            ],
+            "relatedAcsFleets" => [
+                [
+                    "fleetId" => "1270",
+                    "mainFleetId" => "1269",
+                ],
+                [
+                    "fleetId" => "1271",
+                    "mainFleetId" => "1269",
+                ],
+                [
+                    "fleetId" => "1272",
+                    "mainFleetId" => "1269",
+                ],
+            ],
+            "isJoiningThisUnion" => false,
+        ];
+
+        $result = Utils\buildOwnListElement($params);
+
+        $expectedParams = [
+            'FleetNo'                   => 1,
+            'FleetMissionColor'         => 'orange',
+            'FleetMission'              => "United Attack #1",
+            'FleetBehaviour'            => 'In flight to the destination point',
+            'FleetBehaviourTxt'         => '(In flight)',
+            'FleetCount'                => '119',
+            'FleetOriGalaxy'            => '1',
+            'FleetOriSystem'            => '1',
+            'FleetOriPlanet'            => '1',
+            'FleetOriType'              => 'planet',
+            'FleetOriStart'             => '12.06.2020<br/>13:00:05',
+            'FleetDesGalaxy'            => '2',
+            'FleetDesSystem'            => '5',
+            'FleetDesPlanet'            => '10',
+            'FleetDesType'              => 'moon',
+            'FleetDesArrive'            => '12.06.2020<br/>14:15:05',
+            'FleetEndTime'              => '12.06.2020<br/>15:30:05',
+            'FleetHideTargetTime'       => '',
+            'FleetHideTargetorBackTime' => '',
+            'FleetHideComeBackTime'     => '',
+            'FleetHideStayTime'         => $this->hidingClassString,
+            'FleetHideRetreatTime'      => '',
+        ];
+        $expectedParamsData = [
+            'ships' => [
+                '202' => '100',
+                '203' => '10',
+            ],
+            'extraShipsInUnion' => [
+                "202" => 3,
+                "203" => 1,
+                "204" => 5,
             ],
         ];
 
