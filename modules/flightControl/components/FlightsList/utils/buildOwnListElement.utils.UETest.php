32fb4e11e0c37dd500934ceb5bb1a007025f233f
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-04-17 03:08:34 +0200

    GH-139 Create additional test for ACS flight

--- modules/flightControl/components/FlightsList/utils/buildOwnListElement.utils.UETest.php
+++ modules/flightControl/components/FlightsList/utils/buildOwnListElement.utils.UETest.php
@@ -109,4 +109,98 @@ class BuildOwnListElementTestCase extends TestCase {
         $this->assertArraySubset($expectedParams, $result, true);
         $this->assertArraySubset($expectedParamsData, $result['data'], true);
     }
+
+    /**
+     * @test
+     */
+    public function itShouldCreateCorrectElementWithAcsStructure() {
+        $params = [
+            "elementNo" => 1,
+            "fleetEntry" => [
+                "fleet_id" => "1269",
+                "fleet_mess" => "0",
+                "fleet_mission" => "1",
+                "fleet_array" => "202,100;203,10",
+                "fleet_amount" => "100",
+                "fleet_start_time" => "1591971305",
+                "fleet_end_time" => "1591975805",
+                "fleet_end_stay" => "0",
+                "fleet_send_time" => "1591966805",
+                "fleet_start_galaxy" => "1",
+                "fleet_start_system" => "1",
+                "fleet_start_planet" => "1",
+                "fleet_start_type" => "1",
+                "fleet_end_galaxy" => "2",
+                "fleet_end_system" => "5",
+                "fleet_end_planet" => "10",
+                "fleet_end_type" => "3",
+                "fleet_resource_metal" => "0",
+                "fleet_resource_crystal" => "0",
+                "fleet_resource_deuterium" => "0",
+            ],
+            "acsMainFleets" => [
+                "1269" => [
+                    "acsId" => "113",
+                    "hasJoinedFleets" => true,
+                ],
+            ],
+            "currentTimestamp" => 1591968605,
+            "acsUnionsExtraSquads" => [
+                "1269" => [
+                    [
+                        "array" => [
+                            "202" => "1",
+                        ],
+                        "count" => "1",
+                    ],
+                ],
+            ],
+            "relatedAcsFleets" => [
+                [
+                    "fleetId" => "1270",
+                    "mainFleetId" => "1269",
+                ],
+            ],
+            "isJoiningThisUnion" => false,
+        ];
+
+        $result = Utils\buildOwnListElement($params);
+
+        $expectedParams = [
+            'FleetNo'                   => 1,
+            'FleetMissionColor'         => 'orange',
+            'FleetMission'              => "United Attack #1",
+            'FleetBehaviour'            => 'In flight to the destination point',
+            'FleetBehaviourTxt'         => '(In flight)',
+            'FleetCount'                => '101',
+            'FleetOriGalaxy'            => '1',
+            'FleetOriSystem'            => '1',
+            'FleetOriPlanet'            => '1',
+            'FleetOriType'              => 'planet',
+            'FleetOriStart'             => '12.06.2020<br/>13:00:05',
+            'FleetDesGalaxy'            => '2',
+            'FleetDesSystem'            => '5',
+            'FleetDesPlanet'            => '10',
+            'FleetDesType'              => 'moon',
+            'FleetDesArrive'            => '12.06.2020<br/>14:15:05',
+            'FleetEndTime'              => '12.06.2020<br/>15:30:05',
+            'FleetHideTargetTime'       => '',
+            'FleetHideTargetorBackTime' => '',
+            'FleetHideComeBackTime'     => '',
+            'FleetHideStayTime'         => $this->hidingClassString,
+            'FleetHideRetreatTime'      => '',
+        ];
+        $expectedParamsData = [
+            'ships' => [
+                '202' => '100',
+                '203' => '10',
+            ],
+            'extraShipsInUnion' => [
+                "202" => 1,
+            ],
+        ];
+
+        $this->assertArraySubset($expectedParams, $result, true);
+        $this->assertArraySubset($expectedParamsData, $result['data'], true);
+    }
 }
