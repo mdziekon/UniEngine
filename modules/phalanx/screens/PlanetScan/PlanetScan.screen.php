f8be9b11e228f8a4ba9aabbba7652042b14da3d7
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-06-05 03:16:12 +0200

    GH-221 Move the rest of the code into the screen's file

--- modules/phalanx/screens/PlanetScan/PlanetScan.screen.php
+++ modules/phalanx/screens/PlanetScan/PlanetScan.screen.php
@@ -3,25 +3,77 @@
 namespace UniEngine\Engine\Modules\Phalanx\Screens\PlanetScan;
 
 use UniEngine\Engine\Modules\Phalanx;
+use UniEngine\Engine\Modules\Flights;
 
 /**
  * @param Object $props
- * @param array $props['targetDetails']
- * @param arrayRef $props['phalanxMoon']
- * @param number $props['viewingUserId']
+ * @param arrayRef $props['input']
+ * @param arrayRef $props['currentUser']
+ * @param arrayRef $props['currentPlanet']
  * @param number $props['currentTimestamp']
- * @param number $props['scanCost']
- * @param array $props['foundFlights']
  *
  * @return Object $result
  * @return String $result['componentHTML']
  */
 function render($props) {
-    $planetScanResult = Phalanx\Screens\PlanetScan\Components\PlanetScanResult\render($props);
+    global $_Lang;
 
-    return [
-        'componentHTML' => $planetScanResult['componentHTML'],
+    $input = &$props['input'];
+    $currentPlanet = &$props['currentPlanet'];
+    $currentUser = &$props['currentUser'];
+    $currentTimestamp = $props['currentTimestamp'];
+
+    includeLang('overview');
+    includeLang('phalanx');
+
+    $scanCost = PHALANX_DEUTERIUMCOST;
+
+    if (Phalanx\Utils\Helpers\canUserBypassChecks([ 'user' => &$currentUser ])) {
+        $scanCost = 0;
+    }
+
+    $targetCoords = [
+        'galaxy' => (isset($input['galaxy']) ? intval($input['galaxy']) : 0),
+        'system' => (isset($input['system']) ? intval($input['system']) : 0),
+        'planet' => (isset($input['planet']) ? intval($input['planet']) : 0),
     ];
+
+    $tryScanPlanet = Phalanx\Utils\Helpers\tryScanPlanet([
+        'targetCoords' => $targetCoords,
+        'phalanxMoon' => &$currentPlanet,
+        'currentUser' => &$currentUser,
+        'scanCost' => $scanCost,
+    ]);
+
+    if (!$tryScanPlanet['isSuccess']) {
+        $errorMessage = Phalanx\Utils\Errors\mapTryScanPlanetErrorToReadableMessage($tryScanPlanet['error']);
+
+        message($errorMessage, $_Lang['Page_Title']);
+    }
+
+    if ($scanCost > 0) {
+        Phalanx\Utils\Effects\updateMoonFuelOnUsage([
+            'scanCost' => $scanCost,
+            'phalanxMoon' => &$currentPlanet,
+            'currentTimestamp' => $currentTimestamp,
+        ]);
+    }
+
+    $targetDetails = $tryScanPlanet['payload']['targetDetails'];
+    $currentFlights = Flights\Fetchers\fetchCurrentFlights([
+        'targetId' => $targetDetails['id'],
+    ]);
+
+    $planetScanResult = Phalanx\Screens\PlanetScan\Components\PlanetScanResult\render([
+        'targetDetails' => $targetDetails,
+        'phalanxMoon' => &$currentPlanet,
+        'viewingUserId' => $currentUser['id'],
+        'currentTimestamp' => $currentTimestamp,
+        'scanCost' => $scanCost,
+        'foundFlights' => $currentFlights,
+    ]);
+
+    display($planetScanResult['componentHTML'], $_Lang['Page_Title'], false);
 }
 
 ?>
--- phalanx.php
+++ phalanx.php
@@ -13,66 +13,15 @@ include($_EnginePath.'modules/flights/_includes.php');
 include($_EnginePath.'modules/phalanx/_includes.php');
 include($_EnginePath.'includes/functions/GetPhalanxRange.php');
 
-use UniEngine\Engine\Modules\Flights;
 use UniEngine\Engine\Modules\Phalanx;
 
 loggedCheck();
 
-includeLang('overview');
-includeLang('phalanx');
-
-$PageTitle = $_Lang['Page_Title'];
-
-$ThisMoon = &$_Planet;
-$Now = time();
-$ScanCost = PHALANX_DEUTERIUMCOST;
-
-if (Phalanx\Utils\Helpers\canUserBypassChecks([ 'user' => &$_User ])) {
-    $ScanCost = 0;
-}
-
-$targetCoords = [
-    'galaxy' => (isset($_GET['galaxy']) ? intval($_GET['galaxy']) : 0),
-    'system' => (isset($_GET['system']) ? intval($_GET['system']) : 0),
-    'planet' => (isset($_GET['planet']) ? intval($_GET['planet']) : 0),
-];
-
-$tryScanPlanet = Phalanx\Utils\Helpers\tryScanPlanet([
-    'targetCoords' => $targetCoords,
-    'phalanxMoon' => &$ThisMoon,
+Phalanx\Screens\PlanetScan\render([
+    'input' => &$_GET,
     'currentUser' => &$_User,
-    'scanCost' => $ScanCost,
+    'currentPlanet' => &$_Planet,
+    'currentTimestamp' => time(),
 ]);
-
-if (!$tryScanPlanet['isSuccess']) {
-    $errorMessage = Phalanx\Utils\Errors\mapTryScanPlanetErrorToReadableMessage($tryScanPlanet['error']);
-
-    message($errorMessage, $PageTitle);
-}
-
-$targetDetails = $tryScanPlanet['payload']['targetDetails'];
-
-if ($ScanCost > 0) {
-    Phalanx\Utils\Effects\updateMoonFuelOnUsage([
-        'scanCost' => $ScanCost,
-        'phalanxMoon' => &$ThisMoon,
-        'currentTimestamp' => $Now,
-    ]);
-}
-
-$Result_GetFleets = Flights\Fetchers\fetchCurrentFlights([
-    'targetId' => $targetDetails['id'],
-]);
-
-$viewScreen = Phalanx\Screens\PlanetScan\render([
-    'targetDetails' => $targetDetails,
-    'phalanxMoon' => &$ThisMoon,
-    'viewingUserId' => $_User['id'],
-    'currentTimestamp' => $Now,
-    'scanCost' => $ScanCost,
-    'foundFlights' => $Result_GetFleets,
-]);
-
-display($viewScreen['componentHTML'], $PageTitle, false);
 
 ?>
