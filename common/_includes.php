a534f6c13434044bb055b5cd022e258c8c2235a0
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-07-05 00:23:33 +0200

    GH-230 Create galaxy (planet link) routing helper

--- common/_includes.php
+++ common/_includes.php
@@ -6,4 +6,6 @@ include($includePath . './exceptions.php');
 
 include($includePath . './modules/uni/utils/isEmailDomainBanned.util.php');
 
+include($includePath . './utils/routing/galaxy.routing.php');
+
 ?>
