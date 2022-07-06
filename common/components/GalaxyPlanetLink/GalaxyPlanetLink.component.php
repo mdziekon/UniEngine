e9a7fc14c55eff775ed92751dd6dc4cdeef64ea4
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-07-06 03:42:14 +0200

    GH-230 Allow to pass additional link attrs

--- common/components/GalaxyPlanetLink/GalaxyPlanetLink.component.php
+++ common/components/GalaxyPlanetLink/GalaxyPlanetLink.component.php
@@ -10,6 +10,7 @@ use UniEngine\Engine\Common\Utils\Routing;
  * @param number $props['coords']['galaxy']
  * @param number $props['coords']['system']
  * @param number $props['coords']['planet']
+ * @param array? $props['linkAttrs']
  */
 function render($props) {
     $linkText = "[{$props['coords']['galaxy']}:{$props['coords']['system']}:{$props['coords']['planet']}]";
@@ -17,9 +18,16 @@ function render($props) {
     return buildDOMElementHTML([
         'tagName' => 'a',
         'contentHTML' => $linkText,
-        'attrs' => [
-            'href' => Routing\getGalaxyTargetUrl($props['coords']),
-        ],
+        'attrs' => array_merge(
+            [
+                'href' => Routing\getGalaxyTargetUrl($props['coords']),
+            ],
+            (
+                isset($props['linkAttrs']) ?
+                    $props['linkAttrs'] :
+                    []
+            )
+        ),
     ]);
 }
 
