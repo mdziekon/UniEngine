b0cf29ff11a0d46849a3339093f83cd4143af41c
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-08-04 02:38:11 +0200

    GH-243 Phalanx range should not be lower than 0

--- includes/functions/GetPhalanxRange.php
+++ includes/functions/GetPhalanxRange.php
@@ -1,7 +1,10 @@
 <?php
 
-function GetPhalanxRange($PhalanxLevel)
-{
+function GetPhalanxRange ($PhalanxLevel) {
+    if ($PhalanxLevel < 1) {
+        return 0;
+    }
+
     return ($PhalanxLevel * $PhalanxLevel) - 1;
 }
 
