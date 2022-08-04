fdcfd54f280b7d4443de202011c9f8747f88d5d8
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2022-08-04 02:38:50 +0200

    GH-243 Level 1 phalanx should allow to scan current solar system

--- includes/functions/GetPhalanxRange.php
+++ includes/functions/GetPhalanxRange.php
@@ -4,6 +4,9 @@ function GetPhalanxRange ($PhalanxLevel) {
     if ($PhalanxLevel < 1) {
         return 0;
     }
+    if ($PhalanxLevel == 1) {
+        return 1;
+    }
 
     return ($PhalanxLevel * $PhalanxLevel) - 1;
 }
